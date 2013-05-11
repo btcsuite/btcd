93070ef9605721034006485e5bc66868437ec2d6
Dave Collins <davec@conformal.com>
2013-05-11 12:49:22 -0500

    Add negative tests for MsgAlert.
    This commit adds tests for the error paths when encoding and decoding
    MsgAlert.

--- msgalert_test.go
+++ msgalert_test.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"github.com/conformal/btcwire"
 	"github.com/davecgh/go-spew/spew"
+	"io"
 	"reflect"
 	"testing"
 )
@@ -134,6 +135,81 @@ func TestAlertWire(t *testing.T) {
 			t.Errorf("BtcDecode #%d\n got: %s want: %s", i,
 				spew.Sdump(msg), spew.Sdump(test.out))
 			continue
+		}
+	}
+}
+
+// TestAlertWireErrors performs negative tests against wire encode and decode
+// of MsgAlert to confirm error paths work correctly.
+func TestAlertWireErrors(t *testing.T) {
+	pver := btcwire.ProtocolVersion
+
+	baseAlert := btcwire.NewMsgAlert("some payload", "somesig")
+	baseAlertEncoded := []byte{
+		0x0c, // Varint for payload length
+		0x73, 0x6f, 0x6d, 0x65, 0x20, 0x70, 0x61, 0x79,
+		0x6c, 0x6f, 0x61, 0x64, // "some payload"
+		0x07,                                     // Varint for signature length
+		0x73, 0x6f, 0x6d, 0x65, 0x73, 0x69, 0x67, // "somesig"
+	}
+
+	tests := []struct {
+		in       *btcwire.MsgAlert // Value to encode
+		buf      []byte            // Wire encoding
+		pver     uint32            // Protocol version for wire encoding
+		max      int               // Max size of fixed buffer to induce errors
+		writeErr error             // Expected write error
+		readErr  error             // Expected read error
+	}{
+		// Force error in payload length.
+		{baseAlert, baseAlertEncoded, pver, 0, io.ErrShortWrite, io.EOF},
+		// Force error in payload.
+		{baseAlert, baseAlertEncoded, pver, 1, io.ErrShortWrite, io.EOF},
+		// Force error in signature length.
+		{baseAlert, baseAlertEncoded, pver, 13, io.ErrShortWrite, io.EOF},
+		// Force error in signature.
+		{baseAlert, baseAlertEncoded, pver, 14, io.ErrShortWrite, io.EOF},
+	}
+
+	t.Logf("Running %d tests", len(tests))
+	for i, test := range tests {
+		// Encode to wire format.
+		w := newFixedWriter(test.max)
+		err := test.in.BtcEncode(w, test.pver)
+		if reflect.TypeOf(err) != reflect.TypeOf(test.writeErr) {
+			t.Errorf("BtcEncode #%d wrong error got: %v, want: %v",
+				i, err, test.writeErr)
+			continue
+		}
+
+		// For errors which are not of type btcwire.MessageError, check
+		// them for equality.
+		if _, ok := err.(*btcwire.MessageError); !ok {
+			if err != test.writeErr {
+				t.Errorf("BtcEncode #%d wrong error got: %v, "+
+					"want: %v", i, err, test.writeErr)
+				continue
+			}
+		}
+
+		// Decode from wire format.
+		var msg btcwire.MsgAlert
+		r := newFixedReader(test.max, test.buf)
+		err = msg.BtcDecode(r, test.pver)
+		if reflect.TypeOf(err) != reflect.TypeOf(test.readErr) {
+			t.Errorf("BtcDecode #%d wrong error got: %v, want: %v",
+				i, err, test.readErr)
+			continue
+		}
+
+		// For errors which are not of type btcwire.MessageError, check
+		// them for equality.
+		if _, ok := err.(*btcwire.MessageError); !ok {
+			if err != test.readErr {
+				t.Errorf("BtcDecode #%d wrong error got: %v, "+
+					"want: %v", i, err, test.readErr)
+				continue
+			}
 		}
 	}
 }
