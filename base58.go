761970e639ee0eba66a7d4f98f8d6a4804ffc881
John C. Vernaleo <jcv@conformal.com>
2013-06-13 13:09:51 -0400

    Fixed license statment on base58 files.

--- base58.go
+++ base58.go
@@ -1,6 +1,6 @@
-/*
- * Copyright (c) 2013 Conformal Systems LLC.
- */
+// Copyright (c) 2013 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
 
 package btcutil
 
