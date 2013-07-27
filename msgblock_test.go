90bf9e7449bffcf1814f06b625f8de92b7636247
Dave Collins <davec@conformal.com>
2013-07-27 15:32:01 -0500

    Update tests for corrected max block payload size.

--- msgblock_test.go
+++ msgblock_test.go
@@ -35,7 +35,7 @@ func TestBlock(t *testing.T) {
 
 	// Ensure max payload is expected value for latest protocol version.
 	// Num addresses (varInt) + max allowed addresses.
-	wantPayload := uint32(1024 * 1024)
+	wantPayload := uint32(1000000)
 	maxPayload := msg.MaxPayloadLength(pver)
 	if maxPayload != wantPayload {
 		t.Errorf("MaxPayloadLength: wrong max payload length for "+
