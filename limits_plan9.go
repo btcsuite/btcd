21edf9a0b2bc41cd3312e8cc116363c41af743dc
mischief <mischief@offblast.org>
2013-10-16 12:45:53 -0700

    add setLimits stub for plan 9

---
+++ limits_plan9.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package main
+
+// Plan 9 has no process accounting. no-op here
+func setLimits() error {
+	return nil
+}
