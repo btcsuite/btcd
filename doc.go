ccfcbebbdc5c7a9b1406e5878a16c1990c1dc261
Dave Collins <davec@conformal.com>
2013-10-16 10:15:56 -0500

    Add config file information to command doc.

--- doc.go
+++ doc.go
@@ -5,8 +5,20 @@
 /*
 btcd is a full-node bitcoin implementation written in Go.
 
+The default options are sane for most users.  This means btcd will work 'out of
+the box' for most users.  However, there are also a wide variety of flags that
+can be used to control it.
+
+The following section provides a usage overview which enumerates the flags.  An
+interesting point to note is that the long form of all of these options
+(except -C) can be specified in a configuration file that is automatically
+parsed when btcd starts up.  By default, the configuration file is located at
+~/.btcd/btcd.conf on POSIX-style operating systems and %APPDATA%\btcd\btcd.conf
+on Windows.  The -C (--configfile) flag, as shown below, can be used to override
+this location.
+
 Usage:
-        btcd [OPTIONS]
+  btcd [OPTIONS]
 
 The flags are:
   -h, --help           Show this help message
@@ -43,5 +55,6 @@ The flags are:
                        between 1024 and 65536
   -d, --debuglevel=    Logging level {trace, debug, info, warn, error,
                        critical}
+
 */
 package main
