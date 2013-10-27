29f1bf4ae149fb71b6272ed3bf67e61e4b70f540
Dave Collins <davec@conformal.com>
2013-10-27 13:59:19 -0500

    Add tests for new Tx type.
    This commit adds both positive and negative tests for the new Tx type to
    bring its coverage to 100%.
    
    This is part of the ongoing transaction hash optimization effort noted
    in conformal/btcd#25.

---
+++ tx_test.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2013 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package btcutil_test
+
+import (
+	"bytes"
+	"github.com/conformal/btcutil"
+	"github.com/conformal/btcwire"
+	"github.com/davecgh/go-spew/spew"
+	"io"
+	"reflect"
+	"testing"
+)
+
+// TestTx tests the API for Tx.
+func TestTx(t *testing.T) {
+	testTx := Block100000.Transactions[0]
+	tx := btcutil.NewTx(testTx)
+
+	// Ensure we get the same data back out.
+	if msgTx := tx.MsgTx(); !reflect.DeepEqual(msgTx, testTx) {
+		t.Errorf("MsgTx: mismatched MsgTx - got %v, want %v",
+			spew.Sdump(msgTx), spew.Sdump(testTx))
+	}
+
+	// Ensure transaction index set and get work properly.
+	wantIndex := 0
+	tx.SetIndex(0)
+	if gotIndex := tx.Index(); gotIndex != wantIndex {
+		t.Errorf("Index: mismatched index - got %v, want %v",
+			gotIndex, wantIndex)
+	}
+
+	// Hash for block 100,000 transaction 0.
+	wantShaStr := "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87"
+	wantSha, err := btcwire.NewShaHashFromStr(wantShaStr)
+	if err != nil {
+		t.Errorf("NewShaHashFromStr: %v", err)
+	}
+
+	// Request the sha multiple times to test generation and caching.
+	for i := 0; i < 2; i++ {
+		sha := tx.Sha()
+		if !sha.IsEqual(wantSha) {
+			t.Errorf("Sha #%d mismatched sha - got %v, want %v", i,
+				sha, wantSha)
+		}
+	}
+}
+
+// TestNewTxFromBytes tests creation of a Tx from serialized bytes.
+func TestNewTxFromBytes(t *testing.T) {
+	// Serialize the test transaction.
+	testTx := Block100000.Transactions[0]
+	var testTxBuf bytes.Buffer
+	err := testTx.Serialize(&testTxBuf)
+	if err != nil {
+		t.Errorf("Serialize: %v", err)
+	}
+	testTxBytes := testTxBuf.Bytes()
+
+	// Create a new transaction from the serialized bytes.
+	tx, err := btcutil.NewTxFromBytes(testTxBytes)
+	if err != nil {
+		t.Errorf("NewTxFromBytes: %v", err)
+		return
+	}
+
+	// Ensure the generated MsgTx is correct.
+	if msgTx := tx.MsgTx(); !reflect.DeepEqual(msgTx, testTx) {
+		t.Errorf("MsgTx: mismatched MsgTx - got %v, want %v",
+			spew.Sdump(msgTx), spew.Sdump(testTx))
+	}
+}
+
+// TestTxErrors tests the error paths for the Tx API.
+func TestTxErrors(t *testing.T) {
+	// Serialize the test transaction.
+	testTx := Block100000.Transactions[0]
+	var testTxBuf bytes.Buffer
+	err := testTx.Serialize(&testTxBuf)
+	if err != nil {
+		t.Errorf("Serialize: %v", err)
+	}
+	testTxBytes := testTxBuf.Bytes()
+
+	// Truncate the transaction byte buffer to force errors.
+	shortBytes := testTxBytes[:4]
+	_, err = btcutil.NewTxFromBytes(shortBytes)
+	if err != io.EOF {
+		t.Errorf("NewTxFromBytes: did not get expected error - "+
+			"got %v, want %v", err, io.EOF)
+	}
+}
