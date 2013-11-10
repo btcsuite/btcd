fcf9b83256cce165664c1361ba2540dc5a6908b7
Dave Collins <davec@conformal.com>
2013-11-10 12:27:39 -0600

    Add new AppDataDir function.
    This commit adds a new AppDataDir function that can be used to get an
    operating system and application specific directory to be used for storing
    application data.
    
    For example:
    
    dir := AppDataDir("myapp", false)
    
    Would result in:
    
    POSIX (Linux/BSD): ~/.myapp
    Mac OS: $HOME/Library/Application Support/Myapp
    Windows: %LOCALAPPDATA%\Myapp
    Plan 9: $home/myapp
    
    This is work toward conformal/btcd#30.

--- internal_test.go
+++ internal_test.go
@@ -17,3 +17,9 @@ package btcutil
 func (b *Block) SetBlockBytes(buf []byte) {
 	b.serializedBlock = buf
 }
+
+// TstAppDataDir makes the internal appDataDir function available to the test
+// package.
+func TstAppDataDir(goos, appName string, roaming bool) string {
+	return appDataDir(goos, appName, roaming)
+}
