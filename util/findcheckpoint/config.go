dcf2994905e3067d9c81b6dd21ae499d75cf7390
Dave Collins <davec@conformal.com>
2013-11-11 11:52:59 -0600

    Add some missed imports in last commit.

--- util/findcheckpoint/config.go
+++ util/findcheckpoint/config.go
@@ -9,6 +9,7 @@ import (
 	"github.com/conformal/btcdb"
 	_ "github.com/conformal/btcdb/ldb"
 	_ "github.com/conformal/btcdb/sqlite3"
+	"github.com/conformal/btcutil"
 	"github.com/conformal/btcwire"
 	"github.com/conformal/go-flags"
 	"os"
