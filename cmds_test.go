f6c7cf92fd07cf9f75388d7b70bbb701f362e067
Josh Rickmar <jrick@conformal.com>
2014-02-04 17:00:08 -0500

    Make go vet happy.

--- cmds_test.go
+++ cmds_test.go
@@ -177,7 +177,7 @@ var cmdtests = []struct {
 		name: "notifyspent",
 		f: func() (btcjson.Cmd, error) {
 			op := &btcwire.OutPoint{
-				Hash: btcwire.ShaHash{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				Hash: [...]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 					10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 					20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
 					30, 31},
@@ -188,7 +188,7 @@ var cmdtests = []struct {
 		result: &NotifySpentCmd{
 			id: float64(1),
 			OutPoint: &btcwire.OutPoint{
-				Hash: btcwire.ShaHash{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				Hash: [...]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 					10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 					20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
 					30, 31},
