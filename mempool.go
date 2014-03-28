3a03a03a923dd89bd9740f16a68fd60178bd6099
Owain G. Ainsworth <oga@conformal.com>
2014-03-28 00:01:46 +0000

    Kill duff newline.

--- mempool.go
+++ mempool.go
@@ -94,7 +94,6 @@ type txMemPool struct {
 	outpoints     map[btcwire.OutPoint]*btcutil.Tx
 	pennyTotal    float64 // exponentially decaying total for penny spends.
 	lastPennyUnix int64   // unix time of last ``penny spend''
-
 }
 
 // isDust returns whether or not the passed transaction output amount is
