a5169bd8804f816f23198a8a4861fa98a732f559
John C. Vernaleo <jcv@conformal.com>
2014-05-05 09:08:45 -0400

    Remove extraneous character that got added to comment.

--- jsonresults.go
+++ jsonresults.go
@@ -427,7 +427,7 @@ func ReadResultCmd(cmd string, message []byte) (Reply, error) {
 	case "getrawtransaction":
 		// getrawtransaction can either return a JSON object or a
 		// hex-encoded string depending on the verbose flag.  Choose the
-		// right form accordingly.=
+		// right form accordingly.
 		if bytes.IndexByte(objmap["result"], '{') > -1 {
 			var res TxRawResult
 			err = json.Unmarshal(objmap["result"], &res)
