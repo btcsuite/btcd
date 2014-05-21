bff18e5a9305b1f54f900e30c7090d5a19e149d0
Josh Rickmar <jrick@conformal.com>
2014-05-21 17:14:23 -0500

    Introduce better WIF API.
    The old functions DecodePrivateKey and EncodePrivateKey have been
    removed in favor of the DecodeWIF function and the String method of
    the new WIF type.
    
    ok @davecgh

---
+++ wif.go
@@ -0,0 +1,198 @@
+// Copyright (c) 2013, 2014 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package btcutil
+
+import (
+	"bytes"
+	"errors"
+
+	"github.com/conformal/btcec"
+	"github.com/conformal/btcwire"
+)
+
+// ErrMalformedPrivateKey describes an error where a WIF-encoded private
+// key cannot be decoded due to being improperly formatted.  This may occur
+// if the byte length is incorrect or an unexpected magic number was
+// encountered.
+var ErrMalformedPrivateKey = errors.New("malformed private key")
+
+// These constants define the magic numbers used for identifing components
+// of a WIF-encoded private key and the bitcoin address associated with it.
+const (
+	// mainNetKey is the magic number identifying a WIF private key for
+	// the MainNet bitcoin network.
+	mainNetKey byte = 0x80
+
+	// testNetKey is the magic number identifying a WIF private key for
+	// the regression test and TestNet3 bitcoin networks.
+	testNetKey byte = 0xef
+
+	// compressMagic is the magic byte used to identify a WIF encoding for
+	// an address created from a compressed serialized public key.
+	compressMagic byte = 0x01
+)
+
+// WIF contains the individual components described by the Wallet Import Format
+// (WIF).  A WIF string is typically used to represent a private key and its
+// associated address in a way that  may be easily copied and imported into or
+// exported from wallet software.  WIF strings may be decoded into this
+// structure by calling DecodeWIF or created with a user-provided private key
+// by calling NewWIF.
+type WIF struct {
+	// PrivKey is the private key being imported or exported.
+	PrivKey *btcec.PrivateKey
+
+	// CompressPubKey specifies whether the address controlled by the
+	// imported or exported private key was created by hashing a
+	// compressed (33-byte) serialized public key, rather than an
+	// uncompressed (65-byte) one.
+	CompressPubKey bool
+
+	// netID is the bitcoin network identifier byte used when
+	// WIF encoding the private key.
+	netID byte
+}
+
+// NewWIF creates a new WIF structure to export an address and its private key
+// as a string encoded in the Wallet Import Format.  The net argument must be
+// either btcwire.MainNet, btcwire.TestNet3 or btcwire.TestNet.  The compress
+// argument specifies whether the address intended to be imported or exported
+// was created by serializing the public key compressed rather than
+// uncompressed.
+func NewWIF(privKey *btcec.PrivateKey, net btcwire.BitcoinNet, compress bool) (*WIF, error) {
+	// Determine the key's network identifier byte.  The same byte is
+	// shared for TestNet3 and TestNet (the regression test network).
+	switch net {
+	case btcwire.MainNet:
+		return &WIF{privKey, compress, mainNetKey}, nil
+	case btcwire.TestNet, btcwire.TestNet3:
+		return &WIF{privKey, compress, testNetKey}, nil
+	default:
+		return nil, ErrUnknownNet
+	}
+}
+
+// IsForNet returns whether or not the decoded WIF structure is associated
+// with the passed bitcoin network.
+func (w *WIF) IsForNet(net btcwire.BitcoinNet) bool {
+	switch net {
+	case btcwire.MainNet:
+		return w.netID == mainNetKey
+	case btcwire.TestNet, btcwire.TestNet3:
+		return w.netID == testNetKey
+	default:
+		return false
+	}
+}
+
+// DecodeWIF creates a new WIF structure by decoding the string encoding of
+// the import format.
+//
+// The WIF string must be a base58-encoded string of the following byte
+// sequence:
+//
+//  * 1 byte to identify the network, must be 0x80 for mainnet or 0xef for
+//    either testnet3 or the regression test network
+//  * 32 bytes of a binary-encoded, big-endian, zero-padded private key
+//  * Optional 1 byte (equal to 0x01) if the address being imported or exported
+//    was created by taking the RIPEMD160 after SHA256 hash of a serialized
+//    compressed (33-byte) public key
+//  * 4 bytes of checksum, must equal the first four bytes of the double SHA256
+//    of every byte before the checksum in this sequence
+//
+// If the base58-decoded byte sequence does not match this, DecodeWIF will
+// return a non-nil error.  ErrMalformedPrivateKey is returned when the WIF
+// is of an impossible length or the expected compressed pubkey magic number
+// does not equal the expected value of 0x01.  ErrChecksumMismatch is returned
+// if the expected WIF checksum does not match the calculated checksum.
+func DecodeWIF(wif string) (*WIF, error) {
+	decoded := Base58Decode(wif)
+	decodedLen := len(decoded)
+	var compress bool
+
+	// Length of base58 decoded WIF must be 32 bytes + an optional 1 byte
+	// (0x01) if compressed, plus 1 byte for netID + 4 bytes of checksum.
+	switch decodedLen {
+	case 1 + btcec.PrivKeyBytesLen + 1 + 4:
+		if decoded[33] != compressMagic {
+			return nil, ErrMalformedPrivateKey
+		}
+		compress = true
+	case 1 + btcec.PrivKeyBytesLen + 4:
+		compress = false
+	default:
+		return nil, ErrMalformedPrivateKey
+	}
+
+	netID := decoded[0]
+	if netID != mainNetKey && netID != testNetKey {
+		return nil, ErrUnknownNet
+	}
+
+	// Checksum is first four bytes of double SHA256 of the identifier byte
+	// and privKey.  Verify this matches the final 4 bytes of the decoded
+	// private key.
+	var tosum []byte
+	if compress {
+		tosum = decoded[:1+btcec.PrivKeyBytesLen+1]
+	} else {
+		tosum = decoded[:1+btcec.PrivKeyBytesLen]
+	}
+	cksum := btcwire.DoubleSha256(tosum)[:4]
+	if !bytes.Equal(cksum, decoded[decodedLen-4:]) {
+		return nil, ErrChecksumMismatch
+	}
+
+	privKeyBytes := decoded[1 : 1+btcec.PrivKeyBytesLen]
+	privKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), privKeyBytes)
+	return &WIF{privKey, compress, netID}, nil
+}
+
+// String creates the Wallet Import Format string encoding of a WIF structure.
+// See DecodeWIF for a detailed breakdown of the format and requirements of
+// a valid WIF string.
+func (w *WIF) String() string {
+	// Precalculate size.  Maximum number of bytes before base58 encoding
+	// is one byte for the network, 32 bytes of private key, possibly one
+	// extra byte if the pubkey is to be compressed, and finally four
+	// bytes of checksum.
+	encodeLen := 1 + btcec.PrivKeyBytesLen + 4
+	if w.CompressPubKey {
+		encodeLen++
+	}
+
+	a := make([]byte, 0, encodeLen)
+	a = append(a, w.netID)
+	// Pad and append bytes manually, instead of using Serialize, to
+	// avoid another call to make.
+	a = paddedAppend(btcec.PrivKeyBytesLen, a, w.PrivKey.D.Bytes())
+	if w.CompressPubKey {
+		a = append(a, compressMagic)
+	}
+	cksum := btcwire.DoubleSha256(a)[:4]
+	a = append(a, cksum...)
+	return Base58Encode(a)
+}
+
+// SerializePubKey serializes the associated public key of the imported or
+// exported private key in either a compressed or uncompressed format.  The
+// serialization format chosen depends on the value of w.CompressPubKey.
+func (w *WIF) SerializePubKey() []byte {
+	pk := (*btcec.PublicKey)(&w.PrivKey.PublicKey)
+	if w.CompressPubKey {
+		return pk.SerializeCompressed()
+	}
+	return pk.SerializeUncompressed()
+}
+
+// paddedAppend appends the src byte slice to dst, returning the new slice.
+// If the length of the source is smaller than the passed size, leading zero
+// bytes are appended to the dst slice before appending src.
+func paddedAppend(size uint, dst, src []byte) []byte {
+	for i := 0; i < int(size)-len(src); i++ {
+		dst = append(dst, 0)
+	}
+	return append(dst, src...)
+}
---
+++ wif_test.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2013, 2014 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package btcutil_test
+
+import (
+	"testing"
+
+	"github.com/conformal/btcec"
+	. "github.com/conformal/btcutil"
+	"github.com/conformal/btcwire"
+)
+
+func TestEncodeDecodeWIF(t *testing.T) {
+	priv1, _ := btcec.PrivKeyFromBytes(btcec.S256(), []byte{
+		0x0c, 0x28, 0xfc, 0xa3, 0x86, 0xc7, 0xa2, 0x27,
+		0x60, 0x0b, 0x2f, 0xe5, 0x0b, 0x7c, 0xae, 0x11,
+		0xec, 0x86, 0xd3, 0xbf, 0x1f, 0xbe, 0x47, 0x1b,
+		0xe8, 0x98, 0x27, 0xe1, 0x9d, 0x72, 0xaa, 0x1d})
+
+	priv2, _ := btcec.PrivKeyFromBytes(btcec.S256(), []byte{
+		0xdd, 0xa3, 0x5a, 0x14, 0x88, 0xfb, 0x97, 0xb6,
+		0xeb, 0x3f, 0xe6, 0xe9, 0xef, 0x2a, 0x25, 0x81,
+		0x4e, 0x39, 0x6f, 0xb5, 0xdc, 0x29, 0x5f, 0xe9,
+		0x94, 0xb9, 0x67, 0x89, 0xb2, 0x1a, 0x03, 0x98})
+
+	wif1, err := NewWIF(priv1, btcwire.MainNet, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wif2, err := NewWIF(priv2, btcwire.TestNet3, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		wif     *WIF
+		encoded string
+	}{
+		{
+			wif1,
+			"5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ",
+		},
+		{
+			wif2,
+			"cV1Y7ARUr9Yx7BR55nTdnR7ZXNJphZtCCMBTEZBJe1hXt2kB684q",
+		},
+	}
+
+	for _, test := range tests {
+		// Test that encoding the WIF structure matches the expected string.
+		s := test.wif.String()
+		if s != test.encoded {
+			t.Errorf("TestEncodeDecodePrivateKey failed: want '%s', got '%s'",
+				test.encoded, s)
+			continue
+		}
+
+		// Test that decoding the expected string results in the original WIF
+		// structure.
+		w, err := DecodeWIF(test.encoded)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if got := w.String(); got != test.encoded {
+			t.Errorf("NewWIF failed: want '%v', got '%v'", test.wif, got)
+		}
+	}
+}
