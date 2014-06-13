6d79aa5ff12944a90fd7a9658741d0b9b2ec549e
Josh Rickmar <jrick@conformal.com>
2014-06-13 09:46:31 -0500

    Test panic for converting invalid sha strings.
    ok @davecgh

---
+++ internal_test.go
@@ -0,0 +1,14 @@
+package btcnet
+
+import (
+	"testing"
+)
+
+func TestInvalidShaStr(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic for invalid sha string, got nil")
+		}
+	}()
+	newShaHashFromStr("banana")
+}
