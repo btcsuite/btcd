4ca0daacc139d145eab60124a16192265da27acb
Dave Collins <davec@conformal.com>
2014-08-10 16:46:46 -0500

    Add test for private key serialization.
    Also move private key tests to their own file while here.

--- pubkey_test.go
+++ pubkey_test.go
@@ -12,28 +12,11 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-type privKeyTest struct {
-	name string
-	key  []byte
-}
-
 type pubKeyTest struct {
 	name    string
 	key     []byte
 	format  byte
 	isValid bool
-}
-
-var privKeyTests = []privKeyTest{
-	{
-		name: "check curve",
-		key: []byte{
-			0xea, 0xf0, 0x2c, 0xa3, 0x48, 0xc5, 0x24, 0xe6,
-			0x39, 0x26, 0x55, 0xba, 0x4d, 0x29, 0x60, 0x3c,
-			0xd1, 0xa7, 0x34, 0x7d, 0x9d, 0x65, 0xcf, 0xe9,
-			0x3c, 0xe1, 0xeb, 0xff, 0xdc, 0xa2, 0x26, 0x94,
-		},
-	},
 }
 
 var pubKeyTests = []pubKeyTest{
@@ -216,31 +199,6 @@ var pubKeyTests = []pubKeyTest{
 		},
 		isValid: false,
 	},
-}
-
-func TestPrivKeys(t *testing.T) {
-	for _, test := range privKeyTests {
-		priv, pub := btcec.PrivKeyFromBytes(btcec.S256(), test.key)
-
-		_, err := btcec.ParsePubKey(
-			pub.SerializeUncompressed(), btcec.S256())
-		if err != nil {
-			t.Errorf("%s privkey: %v", test.name, err)
-			continue
-		}
-
-		hash := []byte{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9}
-		sig, err := priv.Sign(hash)
-		if err != nil {
-			t.Errorf("%s could not sign: %v", test.name, err)
-			continue
-		}
-
-		if !sig.Verify(hash, pub) {
-			t.Errorf("%s could not verify: %v", test.name, err)
-			continue
-		}
-	}
 }
 
 func TestPubKeys(t *testing.T) {
