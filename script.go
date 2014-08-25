da080e1598fabe49a11c60532ced743c76529a8f
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-08-25 21:09:23 +0100

    Changed privkey to privKey for consistency.

--- script.go
+++ script.go
@@ -1090,21 +1090,21 @@ func MultiSigScript(pubkeys []*btcutil.AddressPubKey, nrequired int) ([]byte, er
 }
 
 // SignatureScript creates an input signature script for tx to spend
-// BTC sent from a previous output to the owner of privkey.  tx must
+// BTC sent from a previous output to the owner of privKey. tx must
 // include all transaction inputs and outputs, however txin scripts are
 // allowed to be filled or empty. The returned script is calculated to
 // be used as the idx'th txin sigscript for tx. subscript is the PkScript
-// of the previous output being used as the idx'th input.  privkey is
+// of the previous output being used as the idx'th input. privKey is
 // serialized in either a compressed or uncompressed format based on
-// compress.  This format must match the same format used to generate
+// compress. This format must match the same format used to generate
 // the payment address, or the script validation will fail.
-func SignatureScript(tx *btcwire.MsgTx, idx int, subscript []byte, hashType byte, privkey *ecdsa.PrivateKey, compress bool) ([]byte, error) {
-	sig, err := signTxOutput(tx, idx, subscript, hashType, privkey)
+func SignatureScript(tx *btcwire.MsgTx, idx int, subscript []byte, hashType byte, privKey *ecdsa.PrivateKey, compress bool) ([]byte, error) {
+	sig, err := signTxOutput(tx, idx, subscript, hashType, privKey)
 	if err != nil {
 		return nil, err
 	}
 
-	pk := (*btcec.PublicKey)(&privkey.PublicKey)
+	pk := (*btcec.PublicKey)(&privKey.PublicKey)
 	var pkData []byte
 	if compress {
 		pkData = pk.SerializeCompressed()
