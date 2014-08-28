c3abafc7eb86f90f17daa064782f4e5faad10ce3
David Hill <dhill@conformal.com>
2014-08-28 10:23:46 -0400

    Println -> Printf when using formatting directives.
    This makes go vet happy.

--- example_test.go
+++ example_test.go
@@ -53,7 +53,7 @@ func ExampleBlockChain_ProcessBlock() {
 		fmt.Printf("Failed to process block: %v\n", err)
 		return
 	}
-	fmt.Println("Block accepted.  Is it an orphan?: %v", isOrphan)
+	fmt.Printf("Block accepted. Is it an orphan?: %v", isOrphan)
 
 	// Output:
 	// Failed to process block: already have block 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
