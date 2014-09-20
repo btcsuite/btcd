8733b9c8df9fd1c6a563cc06cecb301610b87fe5
Dave Collins <davec@conformal.com>
2014-09-20 12:44:51 -0500

    Bump default user agent to 0.2.0.

--- msgversion.go
+++ msgversion.go
@@ -18,7 +18,7 @@ import (
 const MaxUserAgentLen = 2000
 
 // DefaultUserAgent for btcwire in the stack
-const DefaultUserAgent = "/btcwire:0.1.4/"
+const DefaultUserAgent = "/btcwire:0.2.0/"
 
 // MsgVersion implements the Message interface and represents a bitcoin version
 // message.  It is used for a peer to advertise itself as soon as an outbound
