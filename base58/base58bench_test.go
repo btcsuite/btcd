fffd6a2e879d5e7fd4fe2aa04087296e6073ec2d
Dave Collins <davec@conformal.com>
2014-12-22 10:44:01 -0600

    Add benchmarks to base58 package.

---
+++ base58/base58bench_test.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2013-2014 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package base58_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/conformal/btcutil/base58"
+)
+
+func BenchmarkBase58Encode(b *testing.B) {
+	b.StopTimer()
+	data := bytes.Repeat([]byte{0xff}, 5000)
+	b.SetBytes(int64(len(data)))
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		base58.Encode(data)
+	}
+}
+
+func BenchmarkBase58Decode(b *testing.B) {
+	b.StopTimer()
+	data := bytes.Repeat([]byte{0xff}, 5000)
+	encoded := base58.Encode(data)
+	b.SetBytes(int64(len(encoded)))
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		base58.Decode(encoded)
+	}
+}
