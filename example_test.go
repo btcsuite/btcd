41317e871282d37a9673e65a0206672cf944043e
Dave Collins <davec@conformal.com>
2015-01-16 14:00:23 -0600

    Update btcwire import paths to new location.

--- example_test.go
+++ example_test.go
@@ -8,8 +8,8 @@ import (
 	"encoding/hex"
 	"fmt"
 
+	"github.com/btcsuite/btcwire"
 	"github.com/conformal/btcec"
-	"github.com/conformal/btcwire"
 )
 
 // This example demonstrates signing a message with a secp256k1 private key that
