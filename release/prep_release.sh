2c80b4402477551766ac19d605bf696fe68c8aa0
Dave Collins <davec@conformal.com>
2015-02-28 03:33:43 -0600

    Update release script to correct btcctl location.

--- release/prep_release.sh
+++ release/prep_release.sh
@@ -26,7 +26,7 @@ PROJECT=btcd
 PROJECT_UC=$(echo $PROJECT | tr '[:lower:]' '[:upper:]')
 SCRIPT=$(basename $0)
 VERFILE=../version.go
-VERFILES="$VERFILE ../util/btcctl/version.go"
+VERFILES="$VERFILE ../cmd/btcctl/version.go"
 PROJ_CHANGES=../CHANGES
 
 # verify params
