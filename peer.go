b0255e0eb5314ee86b0e96f0ba78de5d25563248
David Hill <dhill@mindcry.org>
2015-06-27 11:04:10 -0400

    Use NewMsgInvSizeHint when we know the size.
    When trickling inv's to peers, allocate for the number of inv's
    in the queue instead of the default 1000.  This should save on
    memory.

--- peer.go
+++ peer.go
@@ -1772,7 +1772,7 @@ out:
 
 			// Create and send as many inv messages as needed to
 			// drain the inventory send queue.
-			invMsg := wire.NewMsgInv()
+			invMsg := wire.NewMsgInvSizeHint(uint(invSendQueue.Len()))
 			for e := invSendQueue.Front(); e != nil; e = invSendQueue.Front() {
 				iv := invSendQueue.Remove(e).(*wire.InvVect)
 
@@ -1787,7 +1787,7 @@ out:
 					waiting = queuePacket(
 						outMsg{msg: invMsg},
 						pendingMsgs, waiting)
-					invMsg = wire.NewMsgInv()
+					invMsg = wire.NewMsgInvSizeHint(uint(invSendQueue.Len()))
 				}
 
 				// Add the inventory that is being relayed to
