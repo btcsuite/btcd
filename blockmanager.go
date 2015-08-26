6bed4a196778acb07e5386ed34b0b235714cfb16
Dave Collins <davec@conformal.com>
2015-08-26 04:54:55 -0500

    database: Replace with new version.
    This commit removes the old database package, moves the new package into
    its place, and updates all imports accordingly.

--- blockmanager.go
+++ blockmanager.go
@@ -15,7 +15,7 @@ import (
 
 	"github.com/btcsuite/btcd/blockchain"
 	"github.com/btcsuite/btcd/chaincfg"
-	database "github.com/btcsuite/btcd/database2"
+	"github.com/btcsuite/btcd/database"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 )
