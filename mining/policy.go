146e64a51e9c0b2226c0c3564f597d8fbdbebf54
Dave Collins <davec@conformal.com>
2015-11-27 17:01:23 -0600

    Address review items.

--- mining/policy.go
+++ mining/policy.go
@@ -22,8 +22,8 @@ type Policy struct {
 	// transactions to be used when generating a block template.
 	BlockPrioritySize uint32
 
-	// TxMinFreeFee is the minimum fee in Satoshi/kB that is required for a
-	// transaction to be treated as free for mining purposes (block template
-	// generation).  This value is in Satoshi/1000 bytes.
+	// TxMinFreeFee is the minimum fee in Satoshi/1000 bytes that is
+	// required for a transaction to be treated as free for mining purposes
+	// (block template generation).
 	TxMinFreeFee btcutil.Amount
 }
