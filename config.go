bd22cb43c6e40e181b1100a60b9941f511bbcc10
Hector Jusforgues <hector.jusforgues@gmail.com>
2015-12-13 19:49:16 +0700

    Add automatic RPC configuration.

--- config.go
+++ config.go
@@ -5,8 +5,12 @@
 package main
 
 import (
+	"bufio"
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"path/filepath"
@@ -384,6 +388,13 @@ func loadConfig() (*config, []string, error) {
 	parser := newConfigParser(&cfg, &serviceOpts, flags.Default)
 	if !(preCfg.RegressionTest || preCfg.SimNet) || preCfg.ConfigFile !=
 		defaultConfigFile {
+
+		if _, err := os.Stat(preCfg.ConfigFile); os.IsNotExist(err) {
+			err := createDefaultConfigFile(preCfg.ConfigFile)
+			if err != nil {
+				btcdLog.Warnf("Error creating a default config file: %v", err)
+			}
+		}
 
 		err := flags.NewIniParser(parser).ParseFile(preCfg.ConfigFile)
 		if err != nil {
@@ -852,6 +863,66 @@ func loadConfig() (*config, []string, error) {
 	}
 
 	return &cfg, remainingArgs, nil
+}
+
+// createDefaultConfig copies the file sample-btcd.conf to the given destination path,
+// and populates it with some randomly generated RPC username and password.
+func createDefaultConfigFile(destinationPath string) error {
+	// Create the destination directory if it does not exists
+	os.MkdirAll(filepath.Dir(destinationPath), 0700)
+
+	// We get the sample config file path, which is in the same directory as this file.
+	_, path, _, _ := runtime.Caller(0)
+	sampleConfigPath := filepath.Join(filepath.Dir(path), "sample-btcd.conf")
+
+	// We generate a random user and password
+	randomBytes := make([]byte, 20)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return err
+	}
+	generatedRpcUser := base64.StdEncoding.EncodeToString(randomBytes)
+
+	_, err = rand.Read(randomBytes)
+	if err != nil {
+		return err
+	}
+	generatedRpcPass := base64.StdEncoding.EncodeToString(randomBytes)
+
+	src, err := os.Open(sampleConfigPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dest, err := os.OpenFile(destinationPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0700)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	// We copy every line from the sample config file to the destination,
+	// only replacing the two lines for rpcuser and rpcpass
+	reader := bufio.NewReader(src)
+	for err != io.EOF {
+		var line string
+		line, err = reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if strings.Contains(line, "rpcuser=") {
+			line = "  rpcuser=" + string(generatedRpcUser)
+		} else if strings.Contains(line, "rpcpass=") {
+			line = "  rpcpass=" + string(generatedRpcPass)
+		}
+
+		if _, err := dest.WriteString(line + "\n"); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // btcdDial connects to the address on the named network using the appropriate
---
+++ config_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"testing"
+)
+
+var (
+	rpcuserRegexp = regexp.MustCompile("(?m)^  rpcuser=.+$")
+	rpcpassRegexp = regexp.MustCompile("(?m)^  rpcpass=.+$")
+)
+
+func TestCreateDefaultConfigFile(t *testing.T) {
+	// Setup a temporary directory
+	tmpDir, err := ioutil.TempDir("", "btcd")
+	if err != nil {
+		t.Fatalf("Failed creating a temporary directory: %v", err)
+	}
+	testpath := filepath.Join(tmpDir, "test.conf")
+	// Clean-up
+	defer func() {
+		os.Remove(testpath)
+		os.Remove(tmpDir)
+	}()
+
+	err = createDefaultConfigFile(testpath)
+
+	if err != nil {
+		t.Fatalf("Failed to create a default config file: %v", err)
+	}
+
+	file, err := os.Open(testpath)
+	if err != nil {
+		t.Fatalf("Failed to open generated default config file: %v", err)
+	}
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("Failed to read generated default config file: %v", err)
+	}
+
+	if !rpcuserRegexp.Match(content) {
+		t.Error("Could not find rpcuser in generated default config file.")
+	}
+
+	if !rpcpassRegexp.Match(content) {
+		t.Error("Could not find rpcpass in generated default config file.")
+	}
+}
