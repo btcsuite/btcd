4bb6939334bf1eb94f178acd5877e31afc83ca7d
Javed Khan <tuxcanfly@gmail.com>
2016-03-15 00:08:13 +0530

    Added CMGR logging; moved dns seed to connmgr

--- log.go
+++ log.go
@@ -11,7 +11,8 @@ import (
 	"github.com/btcsuite/btcd/addrmgr"
 	"github.com/btcsuite/btcd/blockchain"
 	"github.com/btcsuite/btcd/blockchain/indexers"
-	"github.com/btcsuite/btcd/database"
+	"github.com/btcsuite/btcd/connmgr"
+	database "github.com/btcsuite/btcd/database2"
 	"github.com/btcsuite/btcd/peer"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btclog"
@@ -36,6 +37,7 @@ var (
 	bmgrLog    = btclog.Disabled
 	btcdLog    = btclog.Disabled
 	chanLog    = btclog.Disabled
+	cmgrLog    = btclog.Disabled
 	discLog    = btclog.Disabled
 	indxLog    = btclog.Disabled
 	minrLog    = btclog.Disabled
@@ -54,6 +56,7 @@ var subsystemLoggers = map[string]btclog.Logger{
 	"BMGR": bmgrLog,
 	"BTCD": btcdLog,
 	"CHAN": chanLog,
+	"CMGR": cmgrLog,
 	"DISC": discLog,
 	"INDX": indxLog,
 	"MINR": minrLog,
@@ -109,6 +112,10 @@ func useLogger(subsystemID string, logger btclog.Logger) {
 	case "CHAN":
 		chanLog = logger
 		blockchain.UseLogger(logger)
+
+	case "CMGR":
+		cmgrLog = logger
+		connmgr.UseLogger(logger)
 
 	case "DISC":
 		discLog = logger
