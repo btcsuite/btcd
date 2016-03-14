2bd6da6f77590275471ce279e60a01ff2c036de0
Javed Khan <tuxcanfly@gmail.com>
2016-03-15 01:13:56 +0530

    Restore server AddrManager

--- config.go
+++ config.go
@@ -892,6 +892,11 @@ func loadConfig() (*config, []string, error) {
 		return cfg.lookup(host)
 	}
 	connmgr.ChainParams = activeNetParams.Params
+	permanentPeers := cfg.ConnectPeers
+	if len(permanentPeers) == 0 {
+		permanentPeers = cfg.AddPeers
+	}
+	connmgr.PermanentPeers = permanentPeers
 
 	return &cfg, remainingArgs, nil
 }
