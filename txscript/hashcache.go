965d5d8ed35965cc22e7398a7dbcc36be88cead4
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-04-05 13:16:50 -0700

    txscript: add segwit sighash calculation and HashCache integration
    This commit adds the new segwit sighash digest calculation algorithm
    introduced as part of BIP0143. This functionality has been exposed as
    an internal function to be used by the new signing logic (which
    includes the input amount).
    
    Additionally, a new caching structure, the HashCache has been
    introduced. The HashCache is a thread safe cache which allows multiple
    goroutines to safety modify, and query the partial sighashes will allow
    the algorithmic improvement in the digest calculation.

---
+++ txscript/hashcache.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2013-2016 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package txscript
+
+import (
+	"sync"
+
+	"github.com/btcsuite/btcd/wire"
+)
+
+// TxSigHashes houses the partial set of sighashes introduced within BIP0143.
+// This partial set of sighashes may be re-used within each input across a
+// transaction when validating all inputs. As a result, validation complexity
+// for SigHashAll can be reduced by a polynomial factor.
+type TxSigHashes struct {
+	HashPrevOuts wire.ShaHash
+	HashSequence wire.ShaHash
+	HashOutputs  wire.ShaHash
+}
+
+// NewTxSigHashes computes, and returns the cached sighashes a the given
+// transaction.
+func NewTxSigHashes(tx *wire.MsgTx) *TxSigHashes {
+	return &TxSigHashes{
+		HashPrevOuts: calcHashPrevOuts(tx),
+		HashSequence: calcHashSequence(tx),
+		HashOutputs:  calcHashOutputs(tx),
+	}
+}
+
+// HashCache houses a set of partial sighashes keyed by txid. The set of partial
+// sighashes are those introduced within BIP0143 by the new more efficient
+// sighash digest calculation algorithm. Using this threadsafe shared cache,
+// multiple goroutines can safely re-use the pre-computed partial sighashes
+// speeding up validation time amongst all inputs found within a block.
+type HashCache struct {
+	sigHashes map[wire.ShaHash]*TxSigHashes
+
+	sync.RWMutex
+}
+
+// NewHashCache returns a new instance of the HashCache given a maximum number
+// of entries which may exist within it at anytime.
+func NewHashCache(maxSize uint) *HashCache {
+	return &HashCache{
+		sigHashes: make(map[wire.ShaHash]*TxSigHashes, maxSize),
+	}
+}
+
+// AddSigHashes computes, then adds the partial sighashes for the passed
+// transaction.
+func (h *HashCache) AddSigHashes(tx *wire.MsgTx) {
+	h.Lock()
+	defer h.Unlock()
+
+	// TODO(roasbeef): eviction scheme?
+
+	sigHashes := NewTxSigHashes(tx)
+
+	txid := tx.TxSha()
+	h.sigHashes[txid] = sigHashes
+
+	return
+}
+
+// ContainsHashes returns true if the partial sighashes for the passed
+// transaction currently exist within the HashCache, and false otherwise.
+func (h *HashCache) ContainsHashes(tx *wire.MsgTx) bool {
+	h.RLock()
+	defer h.RUnlock()
+
+	txid := tx.TxSha()
+	_, found := h.sigHashes[txid]
+	return found
+}
+
+// GetSigHashes possibly returns the previously cached partial sighashes for
+// the passed transaction. This function also returns an additional boolean
+// value indicating if the sighashes for the passed transaction were found to
+// be present within the HashCache.
+func (h *HashCache) GetSigHashes(tx *wire.MsgTx) (*TxSigHashes, bool) {
+	h.RLock()
+	defer h.RUnlock()
+
+	txid := tx.TxSha()
+	item, found := h.sigHashes[txid]
+
+	return item, found
+}
+
+// PurgeSigHashes removes all partial sighashes from the HashCache belonging to
+// the passed transaction.
+func (h *HashCache) PurgeSigHashes(tx *wire.MsgTx) {
+	h.Lock()
+	defer h.Unlock()
+
+	txid := tx.TxSha()
+	delete(h.sigHashes, txid)
+}
---
+++ txscript/hashcache_test.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2013-2016 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package txscript
