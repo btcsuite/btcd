5e2f2ff4c85cf4a179102eb57d2a70adf6cedd0f
Dave Collins <davec@conformal.com>
2016-04-10 18:27:40 -0500

    chaincfg: Use hex strings for PoW limits.
    Rather than having separate variables for each network's proof-of-work
    limit parameter, introduce a new function to convert a hex string to a
    big.Int and make use of it.  This has a couple of benefits such as being
    easier to visually verify the limit at a glance and removing the need to
    skip between two different areas of the file when looking at the
    parameters.

--- chaincfg/internal_test.go
+++ chaincfg/internal_test.go
@@ -12,3 +12,14 @@ func TestInvalidShaStr(t *testing.T) {
 	}()
 	newShaHashFromStr("banana")
 }
+
+// TestInvalidBigIntStrPanic ensures the newBigFromHex function panics when
+// called with invalid hex.
+func TestInvalidBigIntStrPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic for invalid big int string")
+		}
+	}()
+	newBigFromHex("banana")
+}
