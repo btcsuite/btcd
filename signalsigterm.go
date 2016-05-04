d0289fab727e57f7a03956da80969b8d9d6af781
David Hill <dhill@mindcry.org>
2016-05-04 12:13:22 -0400

    btcd: handle signal SIGTERM
    When an OS reboots or shuts down, it sends all processes SIGTERM before
    sending SIGKILL.  This allows btcd to do a proper shutdown which most
    importantly closes the database.

---
+++ signalsigterm.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2016 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+func init() {
+	signals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+}
