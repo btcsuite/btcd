treyward32/pearl

Support getmempoolinfo.

node/rpcserver.go
@@ -146,6 +146,7 @@ var rpcHandlersBeforeInit = map[string]commandHandler{
 	"getgenerate":           handleGetGenerate,
 	"gethashespersec":       handleGetHashesPerSec,
 	"getinfo":               handleGetInfo,
+	"getmempoolinfo":        handleGetMempoolInfo,
 	"getmininginfo":         handleGetMiningInfo,
 	"getnettotals":          handleGetNetTotals,
 	"getnetworkhashps":      handleGetNetworkHashPS,
@@ -1933,6 +1934,23 @@ func handleGetInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (in
 		Difficulty:      getDifficultyRatio(blkHeader.Bits),
 		TestNet:         cfg.TestNet3,
 		RelayFee:        float64(minTxRelayFee) / btcutil.SatoshiPerBitcoin,
+	}
+
+	return ret, nil
+}
+
+// handleGetMempoolInfo implements the getmempoolinfo command.
+func handleGetMempoolInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	txD := s.server.txMemPool.TxDescs()
+
+	var numBytes int64
+	for _, desc := range txD {
+		numBytes += int64(desc.Tx.MsgTx().SerializeSize())
+	}
+
+	ret := &btcjson.GetMempoolInfoResult{
+		Size:  int64(len(txD)),
+		Bytes: numBytes,
 	}
 
 	return ret, nil

node/rpcserverhelp.go
@@ -288,6 +288,13 @@ var helpDescsEnUS = map[string]string{
 	// GetInfoCmd help.
 	"getinfo--synopsis": "Returns a JSON object containing various state info.",
 
+	// GetMempoolInfoCmd help.
+	"getmempoolinfo--synopsis": "Returns memory pool information",
+
+	// GetMempoolInfoResult help.
+	"getmempoolinforesult-bytes": "Size in bytes of the mempool",
+	"getmempoolinforesult-size":  "Number of transactions in the mempool",
+
 	// GetMiningInfoResult help.
 	"getmininginforesult-blocks":           "Height of the latest best block",
 	"getmininginforesult-currentblocksize": "Size of the latest best block",
@@ -540,6 +547,7 @@ var rpcResultTypes = map[string][]interface{}{
 	"getgenerate":           []interface{}{(*bool)(nil)},
 	"gethashespersec":       []interface{}{(*float64)(nil)},
 	"getinfo":               []interface{}{(*btcjson.InfoChainResult)(nil)},
+	"getmempoolinfo":        []interface{}{(*btcjson.GetMempoolInfoResult)(nil)},
 	"getmininginfo":         []interface{}{(*btcjson.GetMiningInfoResult)(nil)},
 	"getnettotals":          []interface{}{(*btcjson.GetNetTotalsResult)(nil)},
 	"getnetworkhashps":      []interface{}{(*int64)(nil)},

btcsuite/btcd

rpcserver.go
@@ -180,6 +180,7 @@ var rpcHandlersBeforeInit = map[string]commandHandler{
 	"getgenerate":           handleGetGenerate,
 	"gethashespersec":       handleGetHashesPerSec,
 	"getinfo":               handleGetInfo,
+	"getmempoolinfo":        handleGetMempoolInfo,
 	"getmininginfo":         handleGetMiningInfo,
 	"getnettotals":          handleGetNetTotals,
 	"getnetworkhashps":      handleGetNetworkHashPS,
@@ -3329,6 +3330,23 @@ func handleGetInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (in
 		Difficulty:      getDifficultyRatio(blkHeader.Bits),
 		TestNet:         cfg.TestNet,
 		RelayFee:        float64(minTxRelayFee) / dcrutil.AtomsPerCoin,
+	}
+
+	return ret, nil
+}
+
+// handleGetMempoolInfo implements the getmempoolinfo command.
+func handleGetMempoolInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	txD := s.server.txMemPool.TxDescs()
+
+	var numBytes int64
+	for _, desc := range txD {
+		numBytes += int64(desc.Tx.MsgTx().SerializeSize())
+	}
+
+	ret := &dcrjson.GetMempoolInfoResult{
+		Size:  int64(len(txD)),
+		Bytes: numBytes,
 	}
 
 	return ret, nil

rpcserverhelp.go
@@ -383,6 +383,13 @@ var helpDescsEnUS = map[string]string{
 	// GetInfoCmd help.
 	"getinfo--synopsis": "Returns a JSON object containing various state info.",
 
+	// GetMempoolInfoCmd help.
+	"getmempoolinfo--synopsis": "Returns memory pool information",
+
+	// GetMempoolInfoResult help.
+	"getmempoolinforesult-bytes": "Size in bytes of the mempool",
+	"getmempoolinforesult-size":  "Number of transactions in the mempool",
+
 	// GetMiningInfoResult help.
 	"getmininginforesult-blocks":           "Height of the latest best block",
 	"getmininginforesult-currentblocksize": "Size of the latest best block",
@@ -773,6 +780,7 @@ var rpcResultTypes = map[string][]interface{}{
 	"getgenerate":           []interface{}{(*bool)(nil)},
 	"gethashespersec":       []interface{}{(*float64)(nil)},
 	"getinfo":               []interface{}{(*dcrjson.InfoChainResult)(nil)},
+	"getmempoolinfo":        []interface{}{(*dcrjson.GetMempoolInfoResult)(nil)},
 	"getmininginfo":         []interface{}{(*dcrjson.GetMiningInfoResult)(nil)},
 	"getnettotals":          []interface{}{(*dcrjson.GetNetTotalsResult)(nil)},
 	"getnetworkhashps":      []interface{}{(*int64)(nil)},