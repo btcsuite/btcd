d4860d75f08c3960082cbfcc698b9f7b23dae972
David Hill <dhill@mindcry.org>
2016-10-11 10:54:59 -0400

    dcrctl: fix output in --terminal mode (#416)

--- cmd/dcrctl/terminal.go
+++ cmd/dcrctl/terminal.go
@@ -14,7 +14,7 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func execute(quit chan bool, protected *bool, cfg *config, line string, clear *bool, state *terminal.State) {
+func execute(protected *bool, cfg *config, line string, clear *bool) bool {
 	switch line {
 	case "h":
 		fmt.Printf("[h]elp          print this message\n")
@@ -30,16 +30,16 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 	case "q":
 		fallthrough
 	case "quit":
-		quit <- true
+		return true
 	case "p":
 		fallthrough
 	case "protect":
 		if *protected {
 			*protected = false
-			return
+			return false
 		}
 		*protected = true
-		return
+		return false
 	case "c":
 		fallthrough
 	case "clear":
@@ -49,7 +49,7 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 
 		if len(args) < 1 {
 			usage("No command specified")
-			return
+			return false
 		}
 
 		// Ensure the specified method identifies a valid registered command and
@@ -60,13 +60,13 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", method)
 			fmt.Fprintln(os.Stderr, listCmdMessageLocal)
-			return
+			return false
 		}
 		if usageFlags&unusableFlags != 0 {
 			fmt.Fprintf(os.Stderr, "The '%s' command can only be used via "+
 				"websockets\n", method)
 			fmt.Fprintln(os.Stderr, listCmdMessageLocal)
-			return
+			return false
 		}
 
 		// Convert remaining command line args to a slice of interface values
@@ -80,18 +80,16 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 		params := make([]interface{}, 0, len(args[1:]))
 		for _, arg := range args[1:] {
 			if arg == "-" {
-				terminal.Restore(int(os.Stdin.Fd()), state)
 				param, err := bio.ReadString('\n')
-				state, _ = terminal.MakeRaw(int(os.Stdin.Fd()))
 				if err != nil && err != io.EOF {
 					fmt.Fprintf(os.Stderr, "Failed to read data "+
 						"from stdin: %v\n", err)
-					return
+					return false
 				}
 				if err == io.EOF && len(param) == 0 {
 					fmt.Fprintln(os.Stderr, "Not enough lines "+
 						"provided on stdin")
-					return
+					return false
 				}
 				param = strings.TrimRight(param, "\r\n")
 				params = append(params, param)
@@ -113,7 +111,7 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 				fmt.Fprintf(os.Stderr, "%s command: %v (code: %s)\n",
 					method, err, jerr.Code)
 				commandUsage(method)
-				return
+				return false
 			}
 
 			// The error is not a dcrjson.Error and this really should not
@@ -121,7 +119,7 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 			// if it should happen due to a bug in the package.
 			fmt.Fprintf(os.Stderr, "%s command: %v\n", method, err)
 			commandUsage(method)
-			return
+			return false
 		}
 
 		// Marshal the command into a JSON-RPC byte slice in preparation for
@@ -129,7 +127,7 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 		marshalledJSON, err := dcrjson.MarshalCmd(1, cmd)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return
+			return false
 		}
 
 		// Send the JSON-RPC request to the server using the user-specified
@@ -137,7 +135,7 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 		result, err := sendPostRequest(marshalledJSON, cfg)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return
+			return false
 		}
 
 		// Choose how to display the result based on its type.
@@ -145,73 +143,81 @@ func execute(quit chan bool, protected *bool, cfg *config, line string, clear *b
 		if strings.HasPrefix(strResult, "{") || strings.HasPrefix(strResult, "[") {
 			var dst bytes.Buffer
 			if err := json.Indent(&dst, result, "", "  "); err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to format result: %v",
+				fmt.Fprintf(os.Stderr, "Failed to format result: %v\n",
 					err)
-				return
+				return false
 			}
 			fmt.Println(dst.String())
-			return
+			return false
 
 		} else if strings.HasPrefix(strResult, `"`) {
 			var str string
 			if err := json.Unmarshal(result, &str); err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to unmarshal result: %v",
+				fmt.Fprintf(os.Stderr, "Failed to unmarshal result: %v\n",
 					err)
-				return
+				return false
 			}
 			fmt.Println(str)
-			return
+			return false
 
 		} else if strResult != "null" {
 			fmt.Println(strResult)
 		}
 	}
+	return false
 }
 
 func startTerminal(c *config) {
-	fmt.Printf("Starting terminal mode.\n")
-	fmt.Printf("Enter h for [h]elp.\n")
-	fmt.Printf("Enter q for [q]uit.\n")
-	done := make(chan bool)
-	initState, err := terminal.GetState(int(os.Stdin.Fd()))
-	protected := false
-	clear := false
+	var clear, protected bool
+
+	fmt.Println("Starting terminal mode.")
+	fmt.Println("Enter h for [h]elp.")
+	fmt.Println("Enter l for [l]ist of commands.")
+	fmt.Println("Enter q for [q]uit.")
+
+	termState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
-		fmt.Printf("error getting terminal state: %v\n", err.Error())
+		fmt.Fprintf(os.Stderr, "Failed to set raw mode on STDIN: %v\n",
+			err)
 		return
 	}
+	n := terminal.NewTerminal(os.Stdin, "> ")
+	for {
+		var ln string
+		var err error
+		if !protected {
+			ln, err = n.ReadLine()
+		} else {
+			ln, err = n.ReadPassword(">*")
+		}
+		terminal.Restore(int(os.Stdin.Fd()), termState)
+		if err != nil {
+			break
+		}
 
-	go func() {
-		termState, _ := terminal.MakeRaw(int(os.Stdin.Fd()))
-		n := terminal.NewTerminal(os.Stdin, "> ")
-		for {
-			var ln string
-			var err error
-			if !protected {
-				ln, err = n.ReadLine()
-				if err != nil {
-					done <- true
-				}
-			} else {
-				ln, err = n.ReadPassword(">*")
-				if err != nil {
-					done <- true
-				}
+		quit := execute(&protected, c, ln, &clear)
+		if quit {
+			break
+		}
+
+		if clear {
+			fmt.Println("Clearing history...")
+			termState, err = terminal.MakeRaw(int(os.Stdin.Fd()))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to set raw "+
+					"mode on STDIN: %v\n", err)
+				break
 			}
-			execute(done, &protected, c, ln, &clear, termState)
-
-			if clear {
-				fmt.Println("Clearing history...")
-				termState, _ = terminal.MakeRaw(int(os.Stdin.Fd()))
-				n = terminal.NewTerminal(os.Stdin, "> ")
-				clear = false
+			n = terminal.NewTerminal(os.Stdin, "> ")
+			clear = false
+		} else {
+			termState, err = terminal.MakeRaw(int(os.Stdin.Fd()))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to set raw "+
+					"mode on STDIN: %v\n", err)
+				break
 			}
 		}
-	}()
-	select {
-	case <-done:
-		fmt.Printf("exiting...\n")
-		terminal.Restore(int(os.Stdin.Fd()), initState)
-		close(done)
 	}
+	fmt.Println("exiting...")
 }
