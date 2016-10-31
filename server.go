c05ef678b7c2dcb2cff0105cf895e359c2256c92
Javed Khan <tuxcanfly@gmail.com>
2016-11-01 01:39:29 +0530

    server: Cap max outbound in connmgr cfg at max peers

--- server.go
+++ server.go
@@ -2452,9 +2452,13 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 	}
 
 	// Create a connection manager.
+	maxOutbound := defaultMaxOutbound
+	if cfg.MaxPeers < maxOutbound {
+		maxOutbound = cfg.MaxPeers
+	}
 	cmgr, err := connmgr.New(&connmgr.Config{
 		RetryDuration: connectionRetryInterval,
-		MaxOutbound:   defaultMaxOutbound,
+		MaxOutbound:   uint32(maxOutbound),
 		Dial:          dcrdDial,
 		OnConnection:  s.outboundPeerConnected,
 		GetNewAddress: newAddressFunc,
