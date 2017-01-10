ac74deaf680000c263b84095181d022aa5d309cf
pedro martelletto <pedro@ambientworks.net>
2017-01-10 12:53:21 +0000

    Use a harcoded key to generate filters
    Pointed out by alex@. Using a dummy key for now.

--- server.go
+++ server.go
@@ -771,8 +771,7 @@ func (sp *serverPeer) OnGetCBFilter(_ *peer.Peer, msg *wire.MsgGetCBFilter) {
 	P := uint8(20) // collision probability
 
 	for i := 0; i < gcs.KeySize; i += 4 {
-		binary.BigEndian.PutUint32(key[i:],
-		    randomUint32Number(math.MaxUint32))
+		binary.BigEndian.PutUint32(key[i:], uint32(0xcafebabe))
 	}
 
 	filter, err := gcs.BuildGCSFilter(P, key, txHashes)
@@ -1005,24 +1004,6 @@ func randomUint16Number(max uint16) uint16 {
 	// modulus.
 	var randomNumber uint16
 	var limitRange = (math.MaxUint16 / max) * max
-	for {
-		binary.Read(rand.Reader, binary.LittleEndian, &randomNumber)
-		if randomNumber < limitRange {
-			return (randomNumber % max)
-		}
-	}
-}
-
-// randomUint32Number returns a random uint32 in a specified input range.  Note
-// that the range is in zeroth ordering; if you pass it 1800, you will get
-// values from 0 to 1800.
-func randomUint32Number(max uint32) uint32 {
-	// In order to avoid modulo bias and ensure every possible outcome in
-	// [0, max) has equal probability, the random number must be sampled
-	// from a random source that has a range limited to a multiple of the
-	// modulus.
-	var randomNumber uint32
-	var limitRange = (math.MaxUint32 / max) * max
 	for {
 		binary.Read(rand.Reader, binary.LittleEndian, &randomNumber)
 		if randomNumber < limitRange {
