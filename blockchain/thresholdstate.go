f4d6b22c06382fe605d1f80a4b80ab9ba5976632
Marco Peereboom <marco@peereboom.us>
2017-02-05 09:50:25 -0600

    fix copyright

--- blockchain/thresholdstate.go
+++ blockchain/thresholdstate.go
@@ -1,3 +1,4 @@
+// Copyright (c) 2017 The decred developers
 // Copyright (c) 2016 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
