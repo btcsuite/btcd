5c3e0d6454001c8d14fcf06be94381d93991aaea
John C. Vernaleo <jcv@decred.org>
2017-04-24 08:54:03 -0400

    Bump for v1.0.0
    Remove beta label

--- cmd/dcrctl/version.go
+++ cmd/dcrctl/version.go
@@ -17,13 +17,13 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // These constants define the application version and follow the semantic
 // versioning 2.0.0 spec (http://semver.org/).
 const (
-	appMajor uint = 0
-	appMinor uint = 8
-	appPatch uint = 2
+	appMajor uint = 1
+	appMinor uint = 0
+	appPatch uint = 0
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta"
+	appPreRelease = ""
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
