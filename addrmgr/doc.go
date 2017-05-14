40f4997b95ac18e20c5ffc54dc3c84036b1f1bf5
Dave Collins <davec@conformal.com>
2017-05-13 20:50:06 -0500

    addrmgr: Fix a couple of typos in overview doc.

--- addrmgr/doc.go
+++ addrmgr/doc.go
@@ -9,14 +9,14 @@ Address Manager Overview
 
 In order maintain the peer-to-peer Bitcoin network, there needs to be a source
 of addresses to connect to as nodes come and go.  The Bitcoin protocol provides
-a the getaddr and addr messages to allow peers to communicate known addresses
-with each other.  However, there needs to a mechanism to store those results and
+the getaddr and addr messages to allow peers to communicate known addresses with
+each other.  However, there needs to a mechanism to store those results and
 select peers from them.  It is also important to note that remote peers can't
 be trusted to send valid peers nor attempt to provide you with only peers they
 control with malicious intent.
 
 With that in mind, this package provides a concurrency safe address manager for
-caching and selecting peers in a non-determinstic manner.  The general idea is
+caching and selecting peers in a non-deterministic manner.  The general idea is
 the caller adds addresses to the address manager and notifies it when addresses
 are connected, known good, and attempted.  The caller also requests addresses as
 it needs them.
