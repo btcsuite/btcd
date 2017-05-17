acfdf7edd63ace5783932903aedeaf03f2254d07
Josh Rickmar <jrick@companyzero.com>
2017-05-17 13:57:42 -0400

    travis-ci: Actually install and use glide.
    Also switch to gometalinter.  This matches what the rest of the repos
    are using and it has better support for running linters while ignoring
    vendored dependencies.

--- goclean.sh
+++ goclean.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 # The script does automatic checking on a Go package and its sub-packages, including:
 # 1. gofmt         (http://golang.org/cmd/gofmt/)
-# 2. goimports     (https://github.com/bradfitz/goimports)
-# 3. golint        (https://github.com/golang/lint)
-# 4. go vet        (http://golang.org/cmd/vet)
-# 5. test coverage (http://blog.golang.org/cover)
+# 2. golint        (https://github.com/golang/lint)
+# 3. go vet        (http://golang.org/cmd/vet)
 
-set -e
+set -ex
 
 # Automatic checks
-test -z "$(gofmt -l -w .     | tee /dev/stderr)"
-test -z "$(goimports -l -w . | tee /dev/stderr)"
-test -z "$(golint .          | tee /dev/stderr)"
-go vet ./...
+test -z "$(gometalinter -j 8 --disable-all --enable=gofmt --enable=golint --enable=vet --vendor --deadline=10m ./... 2>&1 | egrep -v 'testdata/' | tee /dev/stderr)"
+
 env GORACE="halt_on_error=1" go test -race $(glide nv)
-
-# Run test coverage on each subdirectories and merge the coverage profile.
-
-echo "mode: count" > profile.cov
-
-# Standard go tooling behavior is to ignore dirs with leading underscores.
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
-do
-if ls $dir/*.go &> /dev/null; then
-  go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-  if [ -f $dir/profile.tmp ]; then
-    cat $dir/profile.tmp | tail -n +2 >> profile.cov
-    rm $dir/profile.tmp
-  fi
-fi
-done
-
-go tool cover -func profile.cov
-
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-# goveralls -coverprofile=profile.cov -service=travis-ci
