9dd0f9f9f07f3396520fc50d6c22eba657e5e5bf
David Hill <dhill@mindcry.org>
2017-05-19 11:28:46 -0400

    Remove incorrect assignment (#39)

--- address.go
+++ address.go
@@ -471,8 +471,6 @@ func NewAddressSecpPubKey(serializedPubKey []byte,
 		pkFormat = PKFHybrid
 	}
 
-	pkFormat = PKFCompressed
-
 	return &AddressSecpPubKey{
 		net:          net,
 		pubKeyFormat: pkFormat,
