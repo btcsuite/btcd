b90ee0c98a4cabf78084a897d8837eb970f0f5bf
John C. Vernaleo <jcv@decred.org>
2017-06-07 12:49:54 -0400

    Bump for v1.0.3

--- cmd/dcrctl/version.go
+++ cmd/dcrctl/version.go
@@ -19,7 +19,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 1
 	appMinor uint = 0
-	appPatch uint = 1
+	appPatch uint = 3
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
