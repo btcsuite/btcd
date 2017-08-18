093a098da5dd3937f224a6b1373cc557f9ff967d
Donald Adu-Poku <donald.adu@gmail.com>
2017-08-18 16:12:38 +0000

    dcrec: use hardcoded datasets in tests.

--- dcrec/edwards/curve_test.go
+++ dcrec/edwards/curve_test.go
@@ -7,8 +7,6 @@ package edwards
 import (
 	"encoding/hex"
 	"math/rand"
-	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -85,6 +83,22 @@ func TestXRecovery(t *testing.T) {
 //   extendedToBigAffine
 //   EncodedBytesToBigIntPoint
 func TestAdd(t *testing.T) {
+	pointHexStrIdx := 0
+	pointHexStrSet := []string{
+		"4a3f2684abc42977fe50adbb158a9939cc31b210a7c6e6ea4856395ef3e51bf4",
+		"eb4c9d80865dc40107846fdbc1e8b3ce3647615877a77b88720d2913adf1f0a4",
+		"3e4e5f8276a802b3c8a4bf442a418cc3a435a71ed0f38aa784f9e460d1c57e2e",
+		"647f67801b99bf7eb0c00efbc9b63f4246eba59ff21616e85ecf6139e1006f86",
+		"213b22c341bc07cac961a066e137f1e43e671a497eba7add362e2abb15475de9",
+		"f52f2b3860d5a0a86db4b786dc73d1f3fe29cbfea4b1b0600b58b072d6d25722",
+		"4b6b53496e0d93222fb612f02f688914cb93dea6414d510c92a75812976082ed",
+		"768fbf82d32f02e26fb7e51bce61abbb6081085026d049fdf42efc6fdc0715be",
+		"b66308e6f1080b6f623d8ce4c2537ff3e60d6c5288ae00fdcc9e652ff770d193",
+		"e5bbf34b1d308d393b8fee9e3bba1fd66726ceeba2d8be80d46fd74a4eb9187f",
+		"a172dd13f4eaf3bd0e3501f3c2edca2ceddbbe05e6a5d503b114d6e9e14522e5",
+		"413df123e96ffc6e8d033037cdbe40d70fb9ec17adf547d9f95a2c6e778bb3cb",
+		"b3c815edc658038e31fef3e08190bfdfc63640df5e3b490fb50421cc0380bc21",
+	}
 	curve := new(TwistedEdwardsCurve)
 	curve.InitParam25519()
 	tpcv := testPointConversionVectors()
@@ -99,24 +113,20 @@ func TestAdd(t *testing.T) {
 		if err != nil {
 			continue
 		}
-		point1AsStr := hex.EncodeToString(tpcv[i-1].bIn[:])
 
 		x2, y2, err := curve.EncodedBytesToBigIntPoint(tpcv[i].bIn)
 		// The random point wasn't on the curve.
 		if err != nil {
 			continue
 		}
-		point2AsStr := hex.EncodeToString(tpcv[i].bIn[:])
 
 		x, y := curve.Add(x1, y1, x2, y2)
 		pointEnc := BigIntPointToEncodedBytes(x, y)
 		pointEncAsStr := hex.EncodeToString(pointEnc[:])
-
-		// Test python lib point --> bytes versus our results.
-		args := []string{"testdata/addpoints.py", point1AsStr, point2AsStr}
-		pyHexStr, _ := exec.Command("python", args...).Output()
-		stripped := strings.TrimSpace(string(pyHexStr))
-		assert.Equal(t, pointEncAsStr, string(stripped))
+		// Assert our results.
+		pointHexStr := pointHexStrSet[pointHexStrIdx]
+		assert.Equal(t, pointEncAsStr, pointHexStr)
+		pointHexStrIdx++
 	}
 }
 
