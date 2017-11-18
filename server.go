f138299b0f98155fab497c050ad301641144c1f3
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-11-18 14:08:15 -0800

    server: properly set feeEstimator in newServer

--- server.go
+++ server.go
@@ -2486,6 +2486,7 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 		ChainParams:        s.chainParams,
 		DisableCheckpoints: cfg.DisableCheckpoints,
 		MaxPeers:           cfg.MaxPeers,
+		FeeEstimator:       s.feeEstimator,
 	})
 	if err != nil {
 		return nil, err
