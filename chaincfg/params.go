21e99826326146ddf29be3b142f0de84023e1c06
qshuai <qishuai231@gmail.com>
2018-05-17 15:55:43 +0800

    update dnsseeds for mainnet and testnet

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -227,12 +227,12 @@ var MainNetParams = Params{
 	Net:         wire.MainNet,
 	DefaultPort: "8333",
 	DNSSeeds: []DNSSeed{
-		{"seed.bitcoin.sipa.be", true},
-		{"dnsseed.bluematt.me", true},
-		{"dnsseed.bitcoin.dashjr.org", false},
-		{"seed.bitcoinstats.com", true},
-		{"seed.bitnodes.io", false},
-		{"seed.bitcoin.jonasschnelli.ch", true},
+		{"seed.bitcoinabc.org", true},
+		{"seed-abc.bitcoinforks.org", true},
+		{"btccash-seeder.bitcoinunlimited.info", true},
+		{"seed.bitprim.org", true},
+		{"seed.deadalnix.me", true},
+		{"seeder.criptolayer.net", true},
 	},
 
 	// Chain parameters
@@ -328,7 +328,8 @@ var RegressionNetParams = Params{
 	Name:        "regtest",
 	Net:         wire.TestNet,
 	DefaultPort: "18444",
-	DNSSeeds:    []DNSSeed{},
+	// Regtest mode doesn't have any DNS seeds
+	DNSSeeds: []DNSSeed{},
 
 	// Chain parameters
 	GenesisBlock:             &regTestGenesisBlock,
@@ -403,10 +404,11 @@ var TestNet3Params = Params{
 	Net:         wire.TestNet3,
 	DefaultPort: "18333",
 	DNSSeeds: []DNSSeed{
-		{"testnet-seed.bitcoin.jonasschnelli.ch", true},
-		{"testnet-seed.bitcoin.schildbach.de", false},
-		{"seed.tbtc.petertodd.org", true},
-		{"testnet-seed.bluematt.me", false},
+		{"testnet-seed.bitcoinabc.org", true},
+		{"testnet-seed-abc.bitcoinforks.org", true},
+		{"testnet-seed.bitprim.org", true},
+		{"testnet-seed.deadalnix.me", true},
+		{"testnet-seeder.criptolayer.net", true},
 	},
 
 	// Chain parameters
