93481d7f3aada82ce9942d0a65aaddd704752883
Roy Lee <roylee17@gmail.com>
2018-06-14 20:20:44 -0700

    [lbry] server: update client version to /btcwire:0.5.0/LBRY.GO:0.12.2/
    TODO: double check if lbryd bumps the version.

--- server.go
+++ server.go
@@ -62,7 +62,7 @@ const (
 var (
 	// userAgentName is the user agent name and is used to help identify
 	// ourselves to other bitcoin peers.
-	userAgentName = "btcd"
+	userAgentName = "LBRY.GO"
 
 	// userAgentVersion is the user agent version and is used to help
 	// identify ourselves to other bitcoin peers.
