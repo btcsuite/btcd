fc03bea4fd943b0b0cd8b2155b62427914ee6c9d
qshuai <qishuai231@gmail.com>
2018-06-29 01:56:09 +0800

    txscript: Remove repeated ScriptFlags in StandardVerifyFlags

--- txscript/standard.go
+++ txscript/standard.go
@@ -30,7 +30,6 @@ const (
 		ScriptVerifyDERSignatures |
 		ScriptVerifyStrictEncoding |
 		ScriptVerifyMinimalData |
-		ScriptStrictMultiSig |
 		ScriptDiscourageUpgradableNops |
 		ScriptVerifyCleanStack |
 		ScriptVerifyNullFail |
