38303f36cf1627a61dfeb1238fd8c78f07337d00
Conner Fromknecht <conner@lightning.engineering>
2019-01-17 02:13:54 -0800

    blockchain/merkle_test: add benchmarks

--- blockchain/merkle_test.go
+++ blockchain/merkle_test.go
@@ -5,8 +5,10 @@
 package blockchain
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
 )
 
@@ -19,5 +21,53 @@ func TestMerkle(t *testing.T) {
 	if !wantMerkle.IsEqual(calculatedMerkleRoot) {
 		t.Errorf("BuildMerkleTreeStore: merkle root mismatch - "+
 			"got %v, want %v", calculatedMerkleRoot, wantMerkle)
+	}
+}
+
+func makeHashes(size int) []*chainhash.Hash {
+	var hashes = make([]*chainhash.Hash, size)
+	for i := range hashes {
+		hashes[i] = new(chainhash.Hash)
+	}
+	return hashes
+}
+
+// BenchmarkRollingMerkle benches the RollingMerkleTree while varying the number
+// of leaves pushed to the tree.
+func BenchmarkRollingMerkle(b *testing.B) {
+	sizes := []int{
+		1000,
+		2000,
+		4000,
+		8000,
+		16000,
+		32000,
+	}
+
+	hashes := make([][]*chainhash.Hash, len(sizes))
+	for i, size := range sizes {
+		hashes[i] = makeHashes(size)
+	}
+
+	for i, size := range sizes {
+		name := fmt.Sprintf("%d", size)
+		b.Run(name, func(b *testing.B) {
+			benchmarkRollingMerkleSlice(b, hashes[i])
+		})
+	}
+}
+
+var root chainhash.Hash
+
+func benchmarkRollingMerkleSlice(b *testing.B, hashes []*chainhash.Hash) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		merkle := NewRollingMerkleTree(len(hashes))
+		for _, hash := range hashes {
+			merkle.Push(hash)
+		}
+		root = merkle.Root()
 	}
 }
