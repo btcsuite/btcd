89071af29f4d107104895b1b32cfa8ff1e1d1c71
Conner Fromknecht <conner@lightning.engineering>
2019-01-17 02:13:54 -0800

    blockchain/merkle_test: add benchmarks

--- blockchain/merkle_test.go
+++ blockchain/merkle_test.go
@@ -7,17 +7,46 @@ package blockchain
 import (
 	"testing"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 )
 
 // TestMerkle tests the BuildMerkleTreeStore API.
 func TestMerkle(t *testing.T) {
 	block := btcutil.NewBlock(&Block100000)
-	merkles := BuildMerkleTreeStore(block.Transactions(), false)
-	calculatedMerkleRoot := merkles[len(merkles)-1]
+	calcMerkleRoot := BuildMerkleTreeStore(block.Transactions(), false)
 	wantMerkle := &Block100000.Header.MerkleRoot
-	if !wantMerkle.IsEqual(calculatedMerkleRoot) {
+	if !wantMerkle.IsEqual(calcMerkleRoot) {
 		t.Errorf("BuildMerkleTreeStore: merkle root mismatch - "+
-			"got %v, want %v", calculatedMerkleRoot, wantMerkle)
+			"got %v, want %v", calcMerkleRoot, wantMerkle)
 	}
+}
+
+var root *chainhash.Hash
+
+func BenchmarkMerkle(b *testing.B) {
+	txs := make([]*btcutil.Tx, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		tx := btcutil.NewTx(wire.NewMsgTx(2))
+		tx.Hash()
+		txs = append(txs, tx)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	root = BuildMerkleTreeStore(txs, false)
+}
+
+func BenchmarkRollingMerkle(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	merkle := NewRollingMerkleTree(b.N)
+	for i := 0; i < b.N; i++ {
+		var zeroHash chainhash.Hash
+		merkle.Push(&zeroHash)
+	}
+	root = merkle.Root()
 }
