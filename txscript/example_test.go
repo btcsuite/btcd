ac002d6422e3019180183bd55597f00ed2dc3d77
Dave Collins <davec@conformal.com>
2019-03-13 01:11:03 -0500

    txscript: Introduce zero-alloc script tokenizer.
    This implements an efficient and zero-allocation script tokenizer that
    is exported to both provide a new capability to tokenize scripts to
    external consumers of the API as well as to serve as a base for
    refactoring the existing highly inefficient internal code.
    
    It is important to note that this tokenizer is intended to be used in
    consensus critical code in the future, so it must exactly follow the
    existing semantics.
    
    The current script parsing mechanism used throughout the txscript module
    is to fully tokenize the scripts into an array of internal parsed
    opcodes which are then examined and passed around in order to implement
    virtually everything related to scripts.
    
    While that approach does simplify the analysis of certain scripts and
    thus provide some nice properties in that regard, it is both extremely
    inefficient in many cases, and makes it impossible for external
    consumers of the API to implement any form of custom script analysis
    without manually implementing a bunch of error prone tokenizing code or,
    alternatively, the script engine exposing internal structures.
    
    For example, as shown by profiling the total memory allocations of an
    initial sync, the existing script parsing code allocates a total of
    around 295.12GB, which equates to around 50% of all allocations
    performed.  The zero-alloc tokenizer this introduces will allow that to
    be reduced to virtually zero.
    
    The following is a before and after comparison of tokenizing a large
    script with a high opcode count using the existing code versus the
    tokenizer this introduces for both speed and memory allocations:
    
    benchmark                    old ns/op     new ns/op     delta
    BenchmarkScriptParsing-8     63464         677           -98.93%
    
    benchmark                    old allocs     new allocs     delta
    BenchmarkScriptParsing-8     1              0              -100.00%
    
    benchmark                    old bytes     new bytes     delta
    BenchmarkScriptParsing-8     311299        0             -100.00%
    
    The following is an overview of the changes:
    
    - Introduce new error code ErrUnsupportedScriptVersion
    - Implement zero-allocation script tokenizer
    - Add a full suite of tests to ensure the tokenizer works as intended
      and follows the required consensus semantics
    - Add an example of using the new tokenizer to count the number of
      opcodes in a script
    - Update README.md to include the new example
    - Update script parsing benchmark to use the new tokenizer

--- txscript/example_test.go
+++ txscript/example_test.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2014-2016 The btcsuite developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -179,4 +180,35 @@ func ExampleSignTxOutput() {
 
 	// Output:
 	// Transaction successfully signed
+}
+
+// This example demonstrates creating a script tokenizer instance and using it
+// to count the number of opcodes a script contains.
+func ExampleScriptTokenizer() {
+	// Create a script to use in the example.  Ordinarily this would come from
+	// some other source.
+	hash160 := btcutil.Hash160([]byte("example"))
+	script, err := txscript.NewScriptBuilder().AddOp(txscript.OP_DUP).
+		AddOp(txscript.OP_HASH160).AddData(hash160).
+		AddOp(txscript.OP_EQUALVERIFY).AddOp(txscript.OP_CHECKSIG).Script()
+	if err != nil {
+		fmt.Printf("failed to build script: %v\n", err)
+		return
+	}
+
+	// Create a tokenizer to iterate the script and count the number of opcodes.
+	const scriptVersion = 0
+	var numOpcodes int
+	tokenizer := txscript.MakeScriptTokenizer(scriptVersion, script)
+	for tokenizer.Next() {
+		numOpcodes++
+	}
+	if tokenizer.Err() != nil {
+		fmt.Printf("script failed to parse: %v\n", err)
+	} else {
+		fmt.Printf("script contains %d opcode(s)\n", numOpcodes)
+	}
+
+	// Output:
+	// script contains 5 opcode(s)
 }
