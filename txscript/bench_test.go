38ade2c48f95d97497601463457b797dbd0792a2
Conner Fromknecht <conner@lightning.engineering>
2019-04-19 01:43:18 -0700

    txscript: Add benchmark IsPayToWitnessPubkeyHash

--- txscript/bench_test.go
+++ txscript/bench_test.go
@@ -282,3 +282,18 @@ func BenchmarkIsPushOnlyScript(b *testing.B) {
 		_ = IsPushOnlyScript(script)
 	}
 }
+
+// BenchmarkIsWitnessPubKeyHash benchmarks how long it takes to analyze a very
+// large script to determine if it is a standard witness pubkey hash script.
+func BenchmarkIsWitnessPubKeyHash(b *testing.B) {
+	script, err := genComplexScript()
+	if err != nil {
+		b.Fatalf("failed to create benchmark script: %v", err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_ = IsPayToWitnessPubKeyHash(script)
+	}
+}
