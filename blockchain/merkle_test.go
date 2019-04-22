f2ea7674dc65c8bb844d9b22d81b1f2679e9a50c
Conner Fromknecht <conner@lightning.engineering>
2019-04-22 15:43:17 -0700

    blockchain/merkle_test: add merkle root test for block 46559
    This commit adds an explicit assertion that the merkle root
    is properly computed when an odd number of leaves are present, and a
    left node must be hashed with itself to create a valid root.

--- blockchain/merkle_test.go
+++ blockchain/merkle_test.go
@@ -24,6 +24,36 @@ func TestMerkle(t *testing.T) {
 	}
 }
 
+// TestMerkleBlock46559 computes the merkle root of block 46559. The number of
+// transactions in this blocks forces the case where a left leaf must be hashed
+// with itself, since the block only has three transactions.
+func TestRollingMerkleBlock46559(t *testing.T) {
+	merkle := NewRollingMerkleTree(3)
+
+	coinbase, _ := chainhash.NewHashFromStr(
+		"70e208b60ef5156b5f48d64999115195412b3fec607065099effc17a44af8902",
+	)
+	tx1, _ := chainhash.NewHashFromStr(
+		"5a9e2d301cd7d08b1a920d262d4b0ea4e3fd0a4b4e28c8e104470f031d79f787",
+	)
+	tx2, _ := chainhash.NewHashFromStr(
+		"c945e8e02aaccb74f0a12fac98ffeb57d9e3f235d1819df28705690e1d01312f",
+	)
+
+	merkle.Push(coinbase)
+	merkle.Push(tx1)
+	merkle.Push(tx2)
+
+	root := merkle.Root()
+
+	expRoot, _ := chainhash.NewHashFromStr(
+		"c6f00b209da9234b18f8995aad9aaddd8bc5bcd3d5455d8c87cb942265ab5464",
+	)
+	if root != *expRoot {
+		t.Fatalf("root mismatch, want: %v, got %v", *expRoot, root)
+	}
+}
+
 func makeHashes(size int) []*chainhash.Hash {
 	var hashes = make([]*chainhash.Hash, size)
 	for i := range hashes {
