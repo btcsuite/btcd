915788b8e6e0c65adad447fa2796de544fb0fc6a
JeremyRand <jeremyrand@airmail.cc>
2019-10-09 11:23:05 +0000

    rpcclient: Refactor cookie caching

--- rpcclient/cookiefile.go
+++ rpcclient/cookiefile.go
@@ -8,9 +8,7 @@ package rpcclient
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
-	"time"
 )
 
 func readCookieFile(path string) (username, password string, err error) {
@@ -28,37 +26,4 @@ func readCookieFile(path string) (username, password string, err error) {
 
 	username, password = parts[0], parts[1]
 	return
-}
-
-func cookieRetriever(path string) func() (username, password string, err error) {
-	lastCheckTime := time.Time{}
-	lastModTime := time.Time{}
-
-	curUsername, curPassword := "", ""
-	var curError error
-
-	doUpdate := func() {
-		if !lastCheckTime.IsZero() && time.Now().Before(lastCheckTime.Add(30*time.Second)) {
-			return
-		}
-
-		lastCheckTime = time.Now()
-
-		st, err := os.Stat(path)
-		if err != nil {
-			curError = err
-			return
-		}
-
-		modTime := st.ModTime()
-		if !modTime.Equal(lastModTime) {
-			lastModTime = modTime
-			curUsername, curPassword, curError = readCookieFile(path)
-		}
-	}
-
-	return func() (username, password string, err error) {
-		doUpdate()
-		return curUsername, curPassword, curError
-	}
 }
