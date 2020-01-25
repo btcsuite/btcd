ac4db9dec9b9794e9921bab46f09077dfafd4817
Conner Fromknecht <conner@lightning.engineering>
2020-01-24 19:41:26 -0800

    wire/msgtx: use WriteTxOutBuf in txn serialization

--- wire/msgtx.go
+++ wire/msgtx.go
@@ -761,7 +761,7 @@ func (msg *MsgTx) btcEncode(w io.Writer, pver uint32, enc MessageEncoding,
 	}
 
 	for _, to := range msg.TxOut {
-		err = WriteTxOut(w, pver, msg.Version, to)
+		err = WriteTxOutBuf(w, pver, msg.Version, to, buf)
 		if err != nil {
 			return err
 		}
