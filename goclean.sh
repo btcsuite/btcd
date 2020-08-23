e212ae54861182a9a0c8f84899c334e217c89a3a
Dan Cline <dan@dancline.net>
2020-08-22 20:40:03 -0400

    add clang support to goclean.sh

--- goclean.sh
+++ goclean.sh
@@ -8,6 +8,32 @@
 
 set -ex
 
+# Check if we have gcc, if not then use clang, else exit. More compilers can be
+# added if needed.
+if command -v gcc &> /dev/null
+then
+
+    export CC=gcc
+
+elif command -v clang &> /dev/null
+then
+
+    export CC=clang
+
+else
+
+    echo "neither gcc nor clang are in the PATH (they may not be installed),
+    btcd will not compile due to cgo requiring a C compiler, exiting..."
+    exit
+
+fi
+
+if ! command -v golangci-lint &> /dev/null
+then
+    echo "golangci-lint is not in the PATH, it will not run, exiting..."
+    exit
+fi
+
 go test -tags="rpctest" ./...
 
 # Automatic checks
