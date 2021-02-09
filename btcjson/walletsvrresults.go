db917da50228be8e1afb5efd1426756a406b82a7
Quantum Explorer <quantum@dash.org>
2021-02-09 14:49:01 +0700

    fixed import

--- btcjson/walletsvrresults.go
+++ btcjson/walletsvrresults.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/btcsuite/btcd/txscript"
+	"github.com/dashevo/dashd-go/txscript"
 )
 
 // CreateWalletResult models the result of the createwallet command.
