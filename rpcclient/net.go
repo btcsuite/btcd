540786fda6c6d9328eacf005fa84aef5ed1ec725
Jake Sylvestre <jakesyl@gmail.com>
2021-04-13 09:02:41 -0400

    rpcclient: fix documentation typo

--- rpcclient/net.go
+++ rpcclient/net.go
@@ -90,7 +90,7 @@ func (c *Client) NodeAsync(command btcjson.NodeSubCmd, host string,
 // connect or diconnect a non-persistent one.
 //
 // The connectSubCmd should be set either "perm" or "temp", depending on
-// whether we are targetting a persistent or non-persistent peer. Passing nil
+// whether we are targeting a persistent or non-persistent peer. Passing nil
 // will cause the default value to be used, which currently is "temp".
 func (c *Client) Node(command btcjson.NodeSubCmd, host string,
 	connectSubCmd *string) error {
