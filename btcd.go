67aad53f5eb1c696c5c1a8fb5c61fa54aebc5960
Tim Kuijsten <info+git@netsend.nl>
2022-02-22 22:22:10 +0100

    harden btcd on OpenBSD
    Restrict the available set of system calls to the daemon to the basic
    network and filesystem operations on OpenBSD. Further reduce potential
    harm by limiting file system access to the btcd data dir and the rpc
    files.

--- btcd.go
+++ btcd.go
@@ -18,6 +18,7 @@ import (
 	"github.com/btcsuite/btcd/blockchain/indexers"
 	"github.com/btcsuite/btcd/database"
 	"github.com/btcsuite/btcd/limits"
+	"github.com/btcsuite/btcd/ossec"
 )
 
 const (
@@ -143,6 +144,16 @@ func btcdMain(serverChan chan<- *server) error {
 
 		return nil
 	}
+
+	// The config file is already created if it did not exist and the log
+	// file has already been opened by now so we only need to allow
+	// creating rpc cert and key files if they don't exist.
+	unveilx(cfg.RPCKey, "rwc")
+	unveilx(cfg.RPCCert, "rwc")
+	unveilx(cfg.DataDir, "rwc")
+
+	// drop unveil and tty
+	pledgex("stdio rpath wpath cpath flock dns inet")
 
 	// Create server and start it.
 	server, err := newServer(cfg.Listeners, cfg.AgentBlacklist,
@@ -294,6 +305,26 @@ func loadBlockDB() (database.DB, error) {
 
 	btcdLog.Info("Block database loaded")
 	return db, nil
+}
+
+func unveilx(path string, perms string) {
+	err := ossec.Unveil(path, perms)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unveil failed: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func pledgex(promises string) {
+	err := ossec.PledgePromises(promises)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "pledge failed: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	pledgex("unveil stdio id rpath wpath cpath flock dns inet tty")
 }
 
 func main() {
