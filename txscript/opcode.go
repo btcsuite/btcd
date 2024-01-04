21c1333569fa3d21ccff3e174b42ca22b2468f9d
Afanti <127061691+threewebcode@users.noreply.github.com>
2024-01-05 03:24:36 +0800

    fix: correct the typos [skip ci]
    Use the change to rectify some typos.

--- txscript/opcode.go
+++ txscript/opcode.go
@@ -1278,7 +1278,7 @@ func opcodeIfDup(op *opcode, data []byte, vm *Engine) error {
 		return err
 	}
 
-	// Push copy of data iff it isn't zero
+	// Push copy of data if it isn't zero
 	if asBool(so) {
 		vm.dstack.PushByteArray(so)
 	}
@@ -1983,7 +1983,7 @@ func opcodeCheckSig(op *opcode, data []byte, vm *Engine) error {
 		return err
 	}
 
-	// The signature actually needs needs to be longer than this, but at
+	// The signature actually needs to be longer than this, but at
 	// least 1 byte is needed for the hash type below.  The full length is
 	// checked depending on the script flags and upon parsing the signature.
 	//
