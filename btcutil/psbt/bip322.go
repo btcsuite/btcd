caf859990e3e9a33826a318a4243831ed42c49a3
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2024-03-30 11:42:02 +0200

    btcutil/psbt/bip322.go: add `BuildToSignTx` function

--- btcutil/psbt/bip322.go
+++ btcutil/psbt/bip322.go
@@ -82,3 +82,86 @@ func BuildToSpendTx(message string, outPkScript []byte) (*wire.MsgTx, error) {
 
 	return tx, nil
 }
+
+// BuildToSignTx constructs a transaction to prepare for signing using the
+// specified transaction ID, witness script, and additional parameters. It
+// creates the to_sign transaction according to BIP-322. For more details on
+// BIP-322, see: https://github.com/bitcoin/bips/blob/master/bip-0322.mediawiki
+//
+// Parameters:
+//   - toSpendTxId: The transaction ID of the output to spend.
+//   - witnessScript: The witness script corresponding to the output being
+//	spent.
+//   - isRedeemScript: Whether the witness script should be used as the redeem
+//	script.
+//   - tapInternalKey: The internal key for Taproot if applicable, or nil if 
+//	not used.
+//
+// Returns:
+//   - *Packet: The constructed Partially Signed Bitcoin Transaction (PSBT).
+//   - error: An error if occurred during the construction.
+func BuildToSignTx(toSpendTxId string, witnessScript []byte,
+	isRedeemScript bool, tapInternalKey []byte) (*Packet, error) {
+
+	toSpendTxIdHash, err := chainhash.NewHashFromStr(toSpendTxId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create to_sign transaction in accordance to BIP-322:
+	// https://github.com/bitcoin/bips/blob/master/bip-0322.mediawiki#full
+	toSignTx := &wire.MsgTx{
+		Version:  0,
+		LockTime: 0,
+		TxIn: []*wire.TxIn{{
+			PreviousOutPoint: wire.OutPoint{
+				Hash:  *toSpendTxIdHash,
+				Index: 0,
+			},
+			Sequence: 0,
+		}},
+		TxOut: []*wire.TxOut{{
+			Value:    0,
+			PkScript: []byte{txscript.OP_RETURN},
+		}},
+	}
+
+	pInputs := []PInput{{
+		WitnessUtxo: &wire.TxOut{
+			Value:    0,
+			PkScript: witnessScript,
+		},
+	}}
+
+	pOutputs := []POutput{{}}
+
+	psbt := &Packet{
+		UnsignedTx: toSignTx,
+		Inputs: pInputs,
+		Outputs: pOutputs,
+	}
+
+	// Create an updater for the psbt. This also performs a sanity check.
+	updater, err := NewUpdater(psbt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set redeemScript as witnessScript if isRedeemScript.
+	if isRedeemScript {
+		err = updater.AddInRedeemScript(witnessScript, 0)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Set tapInternalKey if provided.
+	if tapInternalKey != nil {
+		err = updater.AddInTaprootInternalKey(tapInternalKey, 0)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return psbt, nil
+}
