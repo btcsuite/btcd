9f93dc1842557e03387d2856c51c589a494d2737
goodfirm <166383463+goodfirm@users.noreply.github.com>
2024-04-11 00:59:42 +0800

    chore: fix function names in comment (#2163)
    Signed-off-by: goodfirm <fanyishang@yeah.net>

--- cmd/gencerts/gencerts.go
+++ cmd/gencerts/gencerts.go
@@ -90,7 +90,7 @@ func cleanAndExpandPath(path string) string {
 	return filepath.Clean(os.ExpandEnv(path))
 }
 
-// filesExists reports whether the named file or directory exists.
+// fileExists reports whether the named file or directory exists.
 func fileExists(name string) bool {
 	if _, err := os.Stat(name); err != nil {
 		if os.IsNotExist(err) {
