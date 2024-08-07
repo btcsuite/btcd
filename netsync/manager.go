09d04ae50acebbb1f40d13d27e9d2682857f5ead
Calvin Kim <calvin@kcalvinalvin.info>
2024-08-07 16:35:40 +0900

    netsync, main: add ConnectedPeers to Manager
    ConnectedPeers returns all the currently connected peers and any new
    peer that's additionally connected through the returned channel.  This
    method is required for query.Workmanager as it needs ot receive peers
    that it can request blocks from.

--- netsync/manager.go
+++ netsync/manager.go
@@ -246,6 +246,7 @@ type SyncManager struct {
 	nextCheckpoint   *chaincfg.Checkpoint
 	queuedBlocks     map[chainhash.Hash]*blockMsg
 	peerSubscribers  []*peerSubscription
+	connectedPeers   func() []*peerpkg.Peer
 
 	// An optional fee estimator.
 	feeEstimator *mempool.FeeEstimator
@@ -1811,6 +1812,29 @@ type peerSubscription struct {
 	cancel <-chan struct{}
 }
 
+// ConnectedPeers returns all the currently connected peers to the channel
+// and then any additional new peers on connect.
+func (sm *SyncManager) ConnectedPeers() (<-chan query.Peer, func(), error) {
+	peers := sm.connectedPeers()
+	peerChan := make(chan query.Peer, len(peers))
+
+	for _, peer := range peers {
+		if sm.isSyncCandidate(peer) {
+			peerChan <- peer
+		}
+	}
+
+	cancelChan := make(chan struct{})
+	sm.peerSubscribers = append(sm.peerSubscribers, &peerSubscription{
+		peers:  peerChan,
+		cancel: cancelChan,
+	})
+
+	return peerChan, func() {
+		close(cancelChan)
+	}, nil
+}
+
 // New constructs a new SyncManager. Use Start to begin processing asynchronous
 // block, tx, and inv updates.
 func New(config *Config) (*SyncManager, error) {
@@ -1829,6 +1853,7 @@ func New(config *Config) (*SyncManager, error) {
 		quit:            make(chan struct{}),
 		queuedBlocks:    make(map[chainhash.Hash]*blockMsg),
 		feeEstimator:    config.FeeEstimator,
+		connectedPeers:  config.ConnectedPeers,
 	}
 
 	best := sm.chain.BestSnapshot()
