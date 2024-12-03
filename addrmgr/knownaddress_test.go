5585b044ea936dec6119ac84b8b818d60ada96f2
ijunxyz123 <ijunxyz123@gmail.com>
2024-12-03 01:05:03 -0500

    Update knownaddress_test.go

--- addrmgr/knownaddress_test.go
+++ addrmgr/knownaddress_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2015 The btcsuite developers
+addrmgr/knownaddress_test.go// Copyright (c) 2013-2015 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
