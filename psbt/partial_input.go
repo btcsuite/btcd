1282cfe0871ec1fd01fbad5ed2c08912ca305a4e
Oli <gugger@gmail.com>
2024-12-28 17:14:10 +0100

    psbt: add global/input silent payment DLEQ proofs

--- psbt/partial_input.go
+++ psbt/partial_input.go
@@ -29,6 +29,7 @@ type PInput struct {
 	TaprootInternalKey     []byte
 	TaprootMerkleRoot      []byte
 	SilentPaymentShares    []SilentPaymentShare
+	SilentPaymentDLEQs     []SilentPaymentDLEQ
 	Unknowns               []*Unknown
 }
 
@@ -381,6 +382,23 @@ func (pi *PInput) deserialize(r io.Reader) error {
 				pi.SilentPaymentShares, *share,
 			)
 
+		case SilentPaymentDLEQInputType:
+			proof, err := ReadSilentPaymentDLEQ(keyData, value)
+			if err != nil {
+				return err
+			}
+
+			// Duplicate keys are not allowed.
+			for _, x := range pi.SilentPaymentDLEQs {
+				if x.EqualKey(proof) {
+					return ErrDuplicateKey
+				}
+			}
+
+			pi.SilentPaymentDLEQs = append(
+				pi.SilentPaymentDLEQs, *proof,
+			)
+
 		default:
 			// A fall through case for any proprietary types.
 			keyCodeAndData := append(
@@ -598,6 +616,20 @@ func (pi *PInput) serialize(w io.Writer) error {
 			)
 			err := serializeKVPairWithType(
 				w, uint8(SilentPaymentShareInputType), keyBytes,
+				valueBytes,
+			)
+			if err != nil {
+				return err
+			}
+		}
+
+		// Serialize the input's silent payment DLEQ proofs.
+		for _, dleq := range pi.SilentPaymentDLEQs {
+			keyBytes, valueBytes := SerializeSilentPaymentDLEQ(
+				&dleq,
+			)
+			err := serializeKVPairWithType(
+				w, uint8(SilentPaymentDLEQInputType), keyBytes,
 				valueBytes,
 			)
 			if err != nil {
