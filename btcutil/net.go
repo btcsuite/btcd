153bf6d828b7477f2b4992292feeefc995d42020
Calvin Kim <calvin@calvinkim.info>
2025-11-12 15:00:23 +0100

    btcutil: use anet instead of net for android builds
    Golang's net package has been broken for android since android 11
    and the node will not be able to call net.InterfaceAddr() without
    root.
    
    For android builds, we use anet so that the btcd node can be used
    without root.

--- btcutil/net.go
+++ btcutil/net.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-//go:build !appengine
-// +build !appengine
+//go:build !android && !appengine
+// +build !android,!appengine
 
 package btcutil
 
