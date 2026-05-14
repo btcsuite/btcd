73af4d6f8871d46aa089d8d64feaa45276e01f3d
Olaoluwa Osuntokun <laolu32@gmail.com>
2026-05-14 16:22:44 -0700

    btcutil: rename interfaceAddrs to InterfaceAddrs in appengine variant
    The previous commit exported the interfaceAddrs helper in net.go but
    missed the parallel definition in net_noop.go (build tag: appengine),
    which left builds with -tags appengine broken with:
    
        ./certgen.go:79:16: undefined: InterfaceAddrs
    
    Rename the appengine no-op variant to match, restoring the appengine
    build.

--- btcutil/net_noop.go
+++ btcutil/net_noop.go
@@ -11,10 +11,10 @@ import (
 	"net"
 )
 
-// interfaceAddrs returns a list of the system's network interface addresses.
+// InterfaceAddrs returns a list of the system's network interface addresses.
 // It is wrapped here so that we can substitute it for a no-op function that
 // returns an empty slice of net.Addr when building for systems that do not
 // allow access to net.InterfaceAddrs().
-func interfaceAddrs() ([]net.Addr, error) {
+func InterfaceAddrs() ([]net.Addr, error) {
 	return []net.Addr{}, nil
 }
